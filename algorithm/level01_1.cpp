b10ec4f615a0b4ffa3785cfcf22fe5ccb9312d94
dhl <lllee.dh@gmail.com>
2022-07-20 23:12:06 +0900

    algo C++ lv1-6(2)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 using namespace std;
 
-char arr[2];
-
-void INPUT(){
-    int i;
-    cin >> arr[0] >> arr[1];
-}
-
-void OUTPUT(){
-    int i;
-
-    for (i=0; i<2; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
 int main(){
-    
-    INPUT();
-    OUTPUT();
+    char alphas[3];
+
+    for (int i=0; i<3; ++i){
+        cin >> alphas[i];
+    }
+
+    for (int j=0; j<3; ++j){
+        cout << int(alphas[j]) << endl;
+    }
     
     return 0;
 }
