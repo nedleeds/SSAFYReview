e48b2d69f40ee74b2e811d11c7e965ea7ff17aa4
dhl <lllee.dh@gmail.com>
2022-07-20 22:36:27 +0900

    algo C++ lv1-5(9)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main(){
-    int num, i;
-    int arr[6];
-    cin >> num;
-
-    for (i=0; i<6; i++){
-        arr[i] = num*(i+1);
+    int num,s, i;
+    int arr[5];
+    
+    s = 0;
+    for (i=0; i<5; i++){
+        cin >> arr[i];
+        s += arr[i];
     }
 
-    for (i=0; i<6; i++){
-        cout << arr[i] << " ";
-    } 
-    cout << endl;
+    cout << "합은" << s << "입니다" << endl;
 
     return 0;
 }
