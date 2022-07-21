dac0f4b1a6b8d3304c3d6fddc7c955b624c79d6c
dhl <lllee.dh@gmail.com>
2022-07-21 15:10:39 +0900

    algo C++ lv1-6.5(8)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,21 +2,26 @@
 using namespace std;
 
 int main(){
-    char arr[2]; 
+    char arr[5], arr2[5]; 
+    char a, og;
+    cin >> a;
+    og = a;
     
-    for (int i=0; i<2; i++){
-        cin >> arr[i];
-        if ('a' <= arr[i] && arr[i] <= 'z'){
-            arr[i] += 'A'-'a';
-        }else if('A' <= arr[i] && arr[i] <= 'Z'){
-            arr[i] -= 'A'-'a';
-        }
+    for(int i=0; i<5; i++){
+        arr[i] = a;
+        a++;
     }
-
-    for (int j=0; j<2; j++){
-        cout << arr[j] << " ";
+    for(int j=0; j<5; j++){
+        arr2[j] = og;
+        og--;
+    }
+    for(int i=0; i<5; i++){
+        cout << arr[i];
     }
     cout << endl;
-
+    for(int j=0; j<5; j++){
+        cout << arr2[j];
+    }
+    cout << endl;
     return 0;
 }
