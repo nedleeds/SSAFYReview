3eba0ece661056ce0fd7409e143c2fe29b73b6b3
dhl <lllee.dh@gmail.com>
2022-07-21 22:34:55 +0900

    algo C++ lv2-1(7)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 using namespace std;
+void SWAP(int arr[6][2], int *cnt){
+    int tmp;
 
-struct ABC{
-    int x;
-    int y;
-}t;
+    for (int r=0; r<6; r++){
+        tmp = arr[r][0];
+        if (arr[r][0] < arr[r][1]){
+            arr[r][0] = arr[r][1];
+            arr[r][1] = tmp;
+            (*cnt)++;
+        }
+    }
+
+}
 
 int main(){
+    int arr[6][2], cnt=0;
 
-    cin >> t.x >> t.y;
-    cout << t.x + t.y << endl;
+    for (int i=0; i<6; i++){
+        for (int j=0; j<2; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    SWAP(arr, &cnt);
+
+    for (int i=0; i<6; i++){
+        for (int j=0; j<2; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << cnt << "명" << endl;
 
     return 0;
 }
--- algorithm/tempCodeRunnerFile.cpp
+++ algorithm/tempCodeRunnerFile.cpp
@@ -1,3 +1,6 @@
-(int j=0; j<6; j++){
-        cout << arr2[j];
+<3; i++){
+        for (int j=0; j<3; j++){
+            cout << arr[i][j];
+        }
+        cout << endl;
     }
