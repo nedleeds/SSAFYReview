fbf725b11bbabb6718db0a47654900c39db54bff
dhl <lllee.dh@gmail.com>
2022-07-21 21:47:21 +0900

    algo C++ lv1-8.5(9)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -3,25 +3,23 @@ using namespace std;
 
 
 int main(){
-    int arr[6][3];
-    int x, y;
+    char arr[3][6];
+    char x, y;
 
     cin >> x >> y;
 
-    for (int i=0; i<6; i++){
-        if (i<3){
-            for (int j=0; j<3; j++){
-                arr[i][j] = x;
-            }
-        }else{
-            for (int j=0; j<3; j++){
-                arr[i][j] = y;
+    for (int r=0; r<3; r++){
+        for (int c=0; c<6; c++){
+            if (c<4){
+                arr[r][c] = x;
+            }else{
+                arr[r][c] = y;
             }
         }
     }
 
-    for (int r=0; r<6; r++){
-        for (int c=0; c<3; c++){
+    for (int r=0; r<3; r++){
+        for (int c=0; c<6; c++){
             cout << arr[r][c];
         }
         cout << endl;
