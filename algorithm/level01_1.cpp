e20755cbae5d1fef172f77a85a4fd7cc3756a62f
dhl <lllee.dh@gmail.com>
2022-07-21 21:26:21 +0900

    algo C++ lv1-8(11)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[3][4] = {{4, 3, 1, 1},
-                     {3, 1, 2, 1},
-                     {0, 0, 1, 2}};
-
-    int n, cnt = 0;
-    cin >> n;
-
-    for (int i=0; i<3; i++){
-        for (int j=0; j<4; j++){
-            if(arr[i][j] == n){
-                cnt += 1;
-            }
-        }
+void starBox(){
+    for (int i=1; i<20; i=i+2){
+        cout << i << " ";
     }
-    
-    cout << cnt << "개 존재합니다" << endl;
+}
 
+void macDoll(){
+    for (char c='H'; c>='A'; c--){
+        cout << c << " ";
+    }
+}
+
+void copyBean(){
+    for (int j=-5; j<6; j++){
+        cout << j << " ";
+    }
+}
+
+int main(){
+    int price;
+    cin >> price;
+
+    if (3500 <= price && price <= 5000){
+        starBox();
+    }else if(2500 <= price && price < 3500){
+        macDoll();
+    }else{
+        copyBean();
+    }
+    cout << endl;
     return 0;
 }
