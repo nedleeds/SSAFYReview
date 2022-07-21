f3b8bfed1d78239d1dff271daed5fc35f39e9284
dhl <lllee.dh@gmail.com>
2022-07-21 21:30:52 +0900

    algo C++ lv1-8.5(2)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 int main(){
-    int arr[6];
+    char arr[6]={'D', 'T', 'A', 'B', 'W', 'Q'};
+    char a;
+
+    cin >> a;
 
     for (int i=0; i<6; i++){
-        cin >> arr[i];
-    }
-
-    for (int j=0; j<6; j++){
-        if (arr[j] < 5){
-            cout << j << "번은 " << arr[j] << "점 불합격" << endl;
-        }else {
-            cout << j << "번은 " << arr[j] << "점 합격" << endl;
+        if (arr[i]==a){
+            cout << i << "번 INDEX" << endl;
+            break;
         }
     }
 
