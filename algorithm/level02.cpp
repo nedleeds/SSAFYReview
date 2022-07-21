5d669590f722b7a6bd514036265745061168d8e1
dhl <lllee.dh@gmail.com>
2022-07-21 22:37:30 +0900

    algo C++ lv2-1(8)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
 using namespace std;
-void SWAP(int arr[6][2], int *cnt){
-    int tmp;
 
-    for (int r=0; r<6; r++){
-        tmp = arr[r][0];
-        if (arr[r][0] < arr[r][1]){
-            arr[r][0] = arr[r][1];
-            arr[r][1] = tmp;
-            (*cnt)++;
-        }
-    }
-
+void BBQ(int *a, int *b){
+    cout << "합:" << *a + *b << endl;
+    cout << "차:" << *a - *b << endl;
+    cout << "곱:" << *a * *b << endl;
+    cout << "몫:" << *a / *b << endl;
 }
 
 int main(){
-    int arr[6][2], cnt=0;
-
-    for (int i=0; i<6; i++){
-        for (int j=0; j<2; j++){
-            cin >> arr[i][j];
-        }
-    }
-
-    SWAP(arr, &cnt);
-
-    for (int i=0; i<6; i++){
-        for (int j=0; j<2; j++){
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << cnt << "명" << endl;
+    int a, b;
+    cin >> a >> b;
+    BBQ(&a, &b);
 
     return 0;
 }
