58ad4438857627f5d4fa99860389ebd61d3ccfd4
dhl <lllee.dh@gmail.com>
2022-07-21 22:00:45 +0900

    algo C++ lv2-1(3)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,28 +2,27 @@
 using namespace std;
 
 int main(){
-    char arr[3][5] = {{'A', 'B', 'C', 'D', 'E'},
-                      {'E', 'A', 'B', 'A', 'B'},
-                      {'A', 'C', 'D', 'E', 'R'}};
-    char a; 
-    int cnt = 0;
+    char arr[6] = {'A', 'F', 'G', 'A', 'B', 'C'};
+    char a, b;
+    int cnt_a = 0, cnt_b = 0;
 
-    cin >> a;
-    cnt = 0;
-    for (int i=0; i<3; i++){
-        for (int j=0; j<5; j++){
-            if (arr[i][j] == a){
-                cnt++;
-            }
+    cin >> a >> b;
+    
+    for (int i=0; i<6; i++){
+        if (arr[i] == a){
+            cnt_a++;
+        }
+        if(arr[i]==b){
+            cnt_b++;
         }
     }
 
-    if (cnt>=3){
-        cout << "대발견\n";
-    } else if(1<= cnt && cnt <3){
-        cout << "발견\n";
-    } else if (cnt == 0){
-        cout << "미발견\n";
+    if (cnt_a>=1 && cnt_b>=1){
+        cout << "와2개\n";
+    } else if(cnt_a*cnt_b == 0 && cnt_a+cnt_b >= 1){
+        cout << "오1개\n";
+    } else if (cnt_a+cnt_b == 0){
+        cout << "우0개\n";
     }
 
     return 0;
