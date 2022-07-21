d7e8901cc888e71cfef769a816ab529e1d38e9ed
dhl <lllee.dh@gmail.com>
2022-07-21 18:14:51 +0900

    algo C++ lv1-8(1)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void BBQ(int *a){
-    if (0 < *a && *a <5){
-        cout << "초기값" << endl;
-    }else if(6 < *a && *a <10){
-        cout << "중간값" << endl;
-    }else{
-        cout << "알수없는값" << endl;
-    }
-}
-
 int main(){
     int a;
 
     cin >> a;
-    if (a==3 || a==5 || a==7){
-        for (int i=0; i<10; i++){
-            cout << i+1;
+    for (int j=0; j<3; j++){
+        for (int i=0; i<5; i++){
+            cout << a;
         }
         cout << endl;
-    }else if(a==0 || a==8){
-        for (int j=10; j>0; j--){
-            cout << j;
+    }
+    for (int j=0; j<3; j++){
+        for (int i=0; i<3; i++){
+            cout << a;
         }
         cout << endl;
-    }else{
-        BBQ(&a);
     }
 
     return 0;
