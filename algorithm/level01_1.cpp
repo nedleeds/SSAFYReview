333e725901c48fd0ec869478616e7c8d217010cc
dhl <lllee.dh@gmail.com>
2022-07-21 16:10:08 +0900

    algo C++ lv1-6.5(10)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main(){
-    int arr[6] = {5, 4, 1, 2, 7, 8};
-    int cnt;
+    char arr[3];
+    for (int i=0; i<3; i++){
+        cin >> arr[i];
+    }
 
-    cin >> cnt;
-    for (int j=0; j<cnt; j++){
-        for(int i=5; i>=0; i--){
-            cout << arr[i] << " ";
-        }
-        cout << endl;
+    if (arr[0] >= arr[1] && arr[0] >= arr[2]){
+        cout << "옳다" << arr[0] << endl;
+    }
+    else{
+        cout << "옳지않음" << endl;
     }
 
     return 0;
