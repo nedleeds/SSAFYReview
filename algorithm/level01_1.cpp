849b8ad895d7de6ccd5aad0ffbea0023cbc8809e
dhl <lllee.dh@gmail.com>
2022-07-21 21:39:03 +0900

    algo C++ lv1-8.5(6)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 
 int main(){
-    char a, b;
+    char a;
     int n;
 
-    cin >> a >> b >> n;
+    cin >> a >> n;
 
     for (int j=0; j<n; j++){
-        for (char i=a; i<=b; i++){
-            cout << i;
+        for (int i=0; i<n; i++){
+            cout << a;
         }
         cout << endl;
     }
