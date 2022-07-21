1a2e09cfe2669b943613f84f4575ba8e57cc723d
dhl <lllee.dh@gmail.com>
2022-07-21 19:13:39 +0900

    algo C++ lv1-8(1)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 int main(){
-    int arr[3][2], a;
+    int n, i;
     
-    for (int r=0; r<3; r++){
-        for (int c=0; c<2; c++){
-            cin >> a;
-            arr[r][c] = a + 2;
-        }
+    cin >> n;
+
+    i = 1;
+    while (i<=n){
+        cout << i << " ";
+        i++;
     }
-    for (int r=0; r<3; r++){
-        for (int c=0; c<2; c++){
-            cout << arr[r][c] << " ";
-        }
-        cout << endl;
-    }
+    cout << endl;
 
     return 0;
 }
