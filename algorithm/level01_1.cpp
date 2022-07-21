8e08d5c38ebf32c09e7a4857b200b6fc655c2235
dhl <lllee.dh@gmail.com>
2022-07-21 18:51:01 +0900

    algo C++ lv1-7.5(4)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,43 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void input(char* a, char *b){
-    cin >> *a >> *b;
-}
-
-void output(char* a, char *b){
-    int large=0; 
-    int small=0; 
-
-    if ('a' <= *a && *a <='z'){
-        small += 1;
-    }else if('A' <= *a && *a <= 'Z'){
-        large += 1;
-    }
-    if ('a' <= *b && *b <='z'){
-        small += 1;
-    }else if('A' <= *b && *b <= 'Z'){
-        large += 1;
-    }
-
-    if (large==2){
-        cout << "대문자들" << endl;
-    }else if(large==1 && small==1){
-        cout << "대소문자" << endl;
-    }else{
-        for (char c='a'; c<='z'; c++){
-            cout << c;
-        }
-        cout << endl;
-    }
-
-}
-
 int main(){
-    char a, b;
+    char arr[3][5], a;
     
-    input(&a, &b);
-    output(&a, &b);
+    cin >> a;
+
+    for (int i=0; i<3; i++){
+        for (int j=0; j<5; j++){
+            arr[i][j] = a;
+            a++;
+        }
+    }
+
+    cout << char(arr[2][2]+('a'-'A')) << endl;
 
     return 0;
 }
