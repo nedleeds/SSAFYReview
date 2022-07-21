ad6ed7542d21a5fbb23cef924d1587ef6758c3f4
dhl <lllee.dh@gmail.com>
2022-07-21 21:17:24 +0900

    algo C++ lv1-8(8)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void input(char* a){
-    cin >> *a;
-}
+int main(){
+    char arr1[6] = {'#', '_', '#', '_', '#', '#'};
 
-void output(char* a, char arr1[5], char arr2[5]){
-    if ('a'<= *a && *a <='z'){
-        for (int i=0; i<5; i++){
-            cout << arr1[i];
-        }
-    }else if('A' <= *a && *a <= 'Z'){
-        for (int j=0; j<5; j++){
-            cout << arr2[j];
-        }
-    }else if('0' <= *a && *a <= '9'){
-        for (char k='H'; k>='A'; k--){
-            cout << k;
+    for (int i=0; i<6; i++){
+        if (arr1[i]=='#'){
+            cout << "샵";
+        }else if (arr1[i]=='_'){
+            cout << "무";
         }
     }
     cout << endl;
-}
-
-int main(){
-    char arr1[5] = {'B', 'D', '5', 'Q', 'A'};
-    char arr2[5] = {'Q', 'E', 'R', 'E', 'F'};
-    char a;
-
-    input(&a);
-    output(&a, arr1, arr2);
 
 
     return 0;
