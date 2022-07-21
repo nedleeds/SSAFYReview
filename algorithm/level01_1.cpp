b57cc12e063b1efed9f36673d8c45a8a7563d013
dhl <lllee.dh@gmail.com>
2022-07-21 09:54:11 +0900

    algo C++ lv1-6.5(2)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    char a, b;
-    cin >> a >> b;
+    char a;
+    cin >> a;
 
-    cout << "문자\'" << a <<"\'의 아스키코드값은 " << int(a) << endl; 
-    cout << "문자\'" << b <<"\'의 아스키코드값은 " << int(b) << endl; 
-
+    for (char i=a; i>='a';i--){
+        cout << i;
+    }
+    cout << endl;
     return 0;
 }
