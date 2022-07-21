7d460f3f993347a570bd7cc5bd2609cd66cc7071
dhl <lllee.dh@gmail.com>
2022-07-21 21:37:52 +0900

    algo C++ lv1-8.5(5)

--- algorithm/level01_1.cpp
+++ algorithm/level01_1.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void input(char arr[17], char a, char b, char c){
-    for (int i=0; i<7; i++){
-        arr[i] = a;
-    }
-    for (int i=7; i<13; i++){
-        arr[i] = b;
-    }
-    for (int i=13; i<17; i++){
-        arr[i] = c;
-    }
-}
 
 int main(){
-    char arr[17] = {0,};
-    char a, b, c;
+    char a, b;
+    int n;
 
-    cin >> a >> b >> c;
+    cin >> a >> b >> n;
 
-    input(arr, a, b, c);
-
-    for (int i=16; i>=0; i--){
-        cout << arr[i];
+    for (int j=0; j<n; j++){
+        for (char i=a; i<=b; i++){
+            cout << i;
+        }
+        cout << endl;
     }
-    cout << endl;
     return 0;
 }
