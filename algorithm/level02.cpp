6f8914d9a7d33bca8be2381336975cf62fec5126
nedleeds <lllee.dh@gmail.com>
2022-07-22 14:54:15 +0900

    algo lv2-2(3)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,29 +2,27 @@
 
 using namespace std;
 
+int chicken(){
+    int a;
+    cin >> a;
+    return a+10;
+}
+
+char coke(){
+    char a;
+    cin >> a;
+    return a;
+}
+
+void KFC(){
+    int a = chicken();    
+    char b = coke();
+
+    cout << a << b << endl;
+}
+
 int main()
 {
-    int a;
-    int arr[4][4]={0,};
-    
-    cin >> a;
-    
-    if (a%2){
-        for (int i=0, j=3; i<4, j>=0; i++, j--){
-            arr[i][j] = i+1;
-        }
-    }else{
-        for (int i=0, j=0; i < 4; i++,j++){
-            arr[i][j] = i+1;
-        }
-    }
-    
-    for (int i=0; i<4; i++){
-        for (int j=0; j<4; j++){
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
+    KFC();
     return 0;
 }
