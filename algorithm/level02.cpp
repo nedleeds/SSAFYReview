71ba1726c7d009ebefd32cd35b04b967e0016e0d
nedleeds <lllee.dh@gmail.com>
2022-07-22 10:07:20 +0900

    algo lv2-1.5(2)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
     char arr[5];
-    int indices[5] = {0,};
     int cnt = 0;
     
     
@@ -13,14 +12,13 @@ int main()
         cin >> arr[i];
         if (arr[i] == 'A'){
             cnt++;
-            indices[i] = 1;
         }
     }
     
     cout << "문자A는 " << cnt << "개발견" <<endl;
     
     for (int i=0; i<5; i++){
-        if (indices[i]==1){
+        if (arr[i] == 'A'){
             cout << i << "번" << endl;
         }
     }
