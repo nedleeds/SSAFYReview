9895a9548b0bf9e449573c455ee7523b909bc849
nedleeds <lllee.dh@gmail.com>
2022-07-22 15:41:31 +0900

    algo lv2-2(10)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,22 +3,29 @@
 using namespace std;
 
 int main(){
-    int n=1;
-    int arr[4][4]={0,};
+    int col;
+    int x = 1;
+    int arr[3][4]={0};
     
-    for (int c=3; c>=0; c--){
-        for (int r=0; r<4; r++){
-            arr[r][c] = n;
-            n++;
+    cin >> col; 
+    
+    for (int r=2; r>=0; r--){
+        for (int c=3; c>-1; c--){
+            // cout << r << c << endl;
+            arr[r][c] = x++;
+            if (c==col){
+                arr[r][c] = 0;
+            }
         }
     }
     
-    for (int i=0; i<4; i++){
+    for (int i=0; i<3; i++){
         for (int j=0; j<4; j++){
-            cout << arr[i][j] << " " ;
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
+    
 
     return 0;
 }
