d6d85d8defc4a212f9ac7772c4ec451beb9609f9
nedleeds <lllee.dh@gmail.com>
2022-07-22 11:50:15 +0900

    algo lv2-1.5(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,50 +3,25 @@
 using namespace std;
 
 
-int findUpper(char arr[2][3]){
-    int cnt_up = 0;
-    
-    for (int i=0; i<2; i++){
-        for (int j=0; j<3; j++){
-            if ('A' <= arr[i][j] && arr[i][j] <= 'Z'){
-                cnt_up++;    
-            }
-        }
-    }
-    
-    return cnt_up;
-}
-
-int findLower(char arr[2][3]){
-    int cnt_lw = 0;
-    
-    for (int i=0; i<2; i++){
-        for (int j=0; j<3; j++){
-            if ('a' <= arr[i][j] && arr[i][j] <= 'z'){
-                cnt_lw++;    
-            }
-        }
-    }
-    
-    return cnt_lw;
-}
-
 int main()
 {
-    char arr[2][3];
-    int n1 = 0, n2 = 0;
+    int arr[3][3] = {{3, 5, 14},
+                     {2, 3, 9},
+                     {6, 2, 7}};
+                     
+    int n, cnt=0;
     
-    for (int i=0; i<2; i++){
+    cin >> n;
+    
+    for (int i=0; i<3; i++){
         for (int j=0; j<3; j++){
-            cin >> arr[i][j];
+            if (arr[i][j]%n == 0){
+                cnt++;
+            }
         }
     }
 
-    n1 = findUpper(arr);
-    n2 = findLower(arr);
-
-    cout << "대문자" << n1 << "개" << endl;
-    cout << "소문자" << n2 << "개" << endl;
+    cout << cnt;
 
     return 0;
 }
