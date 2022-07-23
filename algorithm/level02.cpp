57ba71a4b0eb5717ea8a70d49efa95b9f87509aa
dhl <lllee.dh@gmail.com>
2022-07-23 22:44:38 +0900

    algo C++ step2-lv13.5(3)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,26 +3,21 @@
 using namespace std;
 
 int main(){
-    string arr = "ABCDEFG";
-    int arr2[7] = {4, 2, 5, 1, 6, 7, 3};
-    int a_i, b_i, s = 0;
-    char a, b;
-
-    cin >> a >> b;
-    a_i = a - 'A';
-    b_i = b - 'A';
-
-    if (a_i > b_i){
-        for (int i=b_i+1; i<a_i; i++){
-            s += arr2[i];
-        }
-    }else{
-        for (int j=a_i+1; j<b_i; j++){
-            s += arr2[j];
-        }
+    int A[5], B[5], C[5];
+    
+    for (int i=0; i<5; i++){
+        cin >> A[i];
     }
-
-    cout << s << endl;
-
+    for (int i=0; i<5; i++){
+        cin >> B[i];
+    }
+    for (int i=0; i<5; i++){
+        cin >> C[i];
+    }
+    for (int i=0; i<5; i++){
+        cout << A[i] * B[i] + C[i]  << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
--- algorithm/tempCodeRunnerFile.cpp
+++ algorithm/tempCodeRunnerFile.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-    string s;
-
-    cout << s.length() << "글자" << endl;
-
-    return 0;
-}
+B[i]
