69852fe1a8c36471070821a2b1dd7bfacda10a10
dhl <lllee.dh@gmail.com>
2022-07-23 23:36:16 +0900

    algo C++ step2-lv14(1)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,24 +2,17 @@
 #include <string>
 using namespace std;
 
-int Find(string *n1, string *n2, string *n3){
-    if (*n1 == *n2  && *n2 == *n3){
-        return 1;
-    }else{
-        return 0;
-    }
-}
-
 int main(){
-    string name1, name2, name3;
-    
-    cin >> name1 >> name2 >> name3;
 
-    if (Find(&name1, &name2, &name3)){
-        cout << "YES" << endl;
-    }else{
-        cout << "NO" << endl;
+    int a, b;
+    cin >> a >> b;
+
+    int i=a;
+    while (i<=b){
+        cout << i << " ";
+        i++;
     }
+    cout << endl;
 
     return 0;
 }
