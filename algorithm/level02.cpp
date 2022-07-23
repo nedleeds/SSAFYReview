8f5cdfd8626a4f471a203f7b27111525a0a5838a
dhl <lllee.dh@gmail.com>
2022-07-23 13:35:40 +0900

    algo C++ step2-lv10.5(11)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void INPUT(int *a){
-    cin >> *a;
-}
-
-void CountDown(int *a){
-    for (int i=*a; i>=1; i--){
-        cout << i << " ";
-    }
-    cout << endl;
-}
-
 int main(){
-    int a;
+    int arr[4][4], i=1;
 
-    INPUT(&a);
-    CountDown(&a);
+    for (int c=0; c<4; c++){
+        for (int r=0; r<4; r++){
+            arr[r][c] = (i*2);
+            i++;
+        }
+    }
+
+    for (int r=0; r<4; r++){
+        for (int c=0; c<4; c++){
+            cout << arr[r][c] << " ";
+        }
+        cout << endl;
+    }
     
     return 0;
 }
