81f18e054544a15f226bd036ae1454744d4bae1d
dhl <lllee.dh@gmail.com>
2022-07-23 20:59:39 +0900

    algo C++ step2-lv12.5(4)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,27 +2,32 @@
 #include <string>
 using namespace std;
 
-char arr[4][3] = {
-    {'D', 'A', 'D'},
-    {'Q', 'W', 'Q'},
-    {'A', 'S', 'D'},
-    {'A', 'S', 'D'},
-};
-
 int main(){
-    char x;
-    int cnt=0;
-
+    int x, arr[5][5]={0};
+    
     cin >> x;
 
-    for (int r=0; r<4; r++){
-        for (int c=0; c<3; c++){
-            if (arr[r][c]==x){
-                cout << "존재" << endl;
-                return 0;
+    for (int r=0; r<5; r++){
+        if (r==0 || r==4){
+            for (int c=0; c<5; c++){
+                arr[r][c] = x;
             }
+        }else{
+            arr[r][0] = x;
+            arr[r][4] = x;
         }
     }
-    cout << "없음" << endl;
+
+    for (int r=0; r<5; r++){
+        for (int c=0; c<5; c++){
+            if (arr[r][c]==0){
+                cout << "_";
+            }else{
+                cout << arr[r][c];
+            }
+        }
+        cout << endl;
+    }
+
     return 0;
 }
