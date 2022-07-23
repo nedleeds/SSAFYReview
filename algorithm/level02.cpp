4817e9c32235a08eebe25e01098c5c9b963ed5e3
dhl <lllee.dh@gmail.com>
2022-07-23 23:01:44 +0900

    algo C++ step2-lv13.5(4)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,22 +2,45 @@
 #include <string>
 using namespace std;
 
+struct Point
+{
+    int M; 
+    int m;
+    int M_r, M_c;
+    int m_r, m_c;
+}p1;
+
+
 int main(){
-    int A[5], B[5], C[5];
+    int arr[4][4] = {
+        {3, 4, 1, 6},
+        {3, 5, 3, 6},
+        {0, 0, 0, 0},
+        {5, 4, 6, 0}
+    };
+
+    for (int c=0; c<4; c++){
+        cin >> arr[2][c];
+    }
     
-    for (int i=0; i<5; i++){
-        cin >> A[i];
+    p1.M = 0;
+    p1.m = 9999;
+    for (int r=0; r<4; r++){
+        for (int c=0; c<4; c++){
+            if (p1.M < arr[r][c]){
+                p1.M = arr[r][c];
+                p1.M_r = r;
+                p1.M_c = c;
+            }else if(p1.m > arr[r][c]){
+                p1.m = arr[r][c];
+                p1.m_r = r;
+                p1.m_c = c;
+            }
+        }
     }
-    for (int i=0; i<5; i++){
-        cin >> B[i];
-    }
-    for (int i=0; i<5; i++){
-        cin >> C[i];
-    }
-    for (int i=0; i<5; i++){
-        cout << A[i] * B[i] + C[i]  << " ";
-    }
-    cout << endl;
     
+    cout << "MAX=" << p1.M << "(" << p1.M_r << "," << p1.M_c << ")" << endl;
+    cout << "MIN=" << p1.m << "(" << p1.m_r << "," << p1.m_c << ")" << endl;
+
     return 0;
 }
