f3d3f85c8dbe75b8869a7c5d2a01fe5f04f4e9b8
dhl <lllee.dh@gmail.com>
2022-07-23 23:41:37 +0900

    algo C++ step2-lv14(2)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -4,13 +4,20 @@ using namespace std;
 
 int main(){
 
-    int a, b;
-    cin >> a >> b;
+    int arr[5][4];
 
-    int i=a;
-    while (i<=b){
-        cout << i << " ";
-        i++;
+    for (int r=0; r<5; r++){
+        for (int c=0; c<4; c++){
+            cin >> arr[r][c];
+        }
+    }
+
+    for (int r=0; r<5; r++){
+        int s = 0;
+        for (int c=0; c<4; c++){
+            s += arr[r][c];
+        }
+        cout << s << " ";
     }
     cout << endl;
 
