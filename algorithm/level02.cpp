f5f8efb69886fcfb27f58e0f1321bebf96abcd74
dhl <lllee.dh@gmail.com>
2022-07-23 17:24:45 +0900

    algo C++ step2-lv11.5(5)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
 using namespace std;
 
-char arr[3][5] = {
-    {'a', 'b', 'a', 'c', 'z'},
-    {'c', 't', 'a', 'c', 'd'},
-    {'c', 'c', 'c', 'c', 'a'}
-};
+int main(){
+    int arr[2][3];
 
-int Count(char *x){
-    int cnt=0;
-    for (int r=0; r<3; r++){
-        for (int c=0; c<5; c++){
-            if (arr[r][c] == *x){
-                cnt++;
-            }
+    for (int r=0; r<2; r++){
+        for (int c=0; c<3; c++){
+            cin >> arr[r][c];
         }
     }
-    return cnt;
-}
 
-int main(){
-    char c;
-    int cnt;
-
-    cin >> c;
-    cnt = Count(&c);
-
-    if ( cnt >= 7 ){
-        cout << "세상에" << endl;
-    } else if ( cnt >= 5 ){
-        cout << "와우" << endl;
-    } else if ( cnt >= 3 ){
-        cout << "이야" << endl;
-    } else {
-        cout << "이런" << endl;
+    for (int r=0; r<2; r++){
+        for (int c=0; c<3; c++){
+            if (arr[r][c] == 0){
+                cout << '#';
+            }else{
+                cout << arr[r][c];
+            }
+        }
+        cout << endl;
     }
 
     return 0;
