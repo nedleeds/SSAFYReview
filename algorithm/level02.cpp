7bfbbb65fda0ef96fb906c711c889dd27b833e0a
dhl <lllee.dh@gmail.com>
2022-07-23 14:50:53 +0900

    algo C++ step2-lv11(3)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int sum(int *a, int *b){
-    int s = *a + *b;
-    return s;
-}
-
-int sub(int *a, int *b){
-    int s = *a - *b;
-    return s;
-}
-
-void print(int *a, int *b){
-    cout << "합:" << *a << endl;
-    cout << "차:" << *b << endl;
-}
-
 int main(){
     
-    int a, b, s1, s2;
-    cin >> a >> b;
+    int a;
+    char gd;
+
+    int *p = &a;
+    char *c = &gd;
+
+    cin >> a >> gd;
     
-    s1 = sum(&a, &b);
-    s2 = sub(&a, &b);
-    print(&s1, &s2);
+    cout << *p << " ";
+    cout << *c << endl;
     
     return 0;
 }
