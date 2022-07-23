c07db38a236eb76f759cf4d5330400ca102c4448
dhl <lllee.dh@gmail.com>
2022-07-23 15:43:04 +0900

    algo C++ step2-lv11(7)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,23 +3,20 @@ using namespace std;
 
 int main(){
     
-    int arr[7] = {3, 4, 1, 3, 2, 7, 3};
-    int n, flag=0;
-
-    cin >> n;
+    int arr[7];
+    int MIN=9999, MAX=0;
 
     for (int i=0; i<7; i++){
-        if(arr[i]==n){
-            flag = 1;
+        cin >> arr[i];
+        if (arr[i] > MAX){
+            MAX = arr[i];
+        }else if (arr[i] < MIN){
+            MIN = arr[i];
         }
     }
 
-    if (flag){
-        cout << "발견" << endl;
-    }else{
-        cout << "미발견" << endl;
-    }
-
+    cout << "MAX=" << MAX << endl;
+    cout << "MIN=" << MIN << endl;
     
     return 0;
 }
