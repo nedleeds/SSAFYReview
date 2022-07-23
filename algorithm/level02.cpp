bd574f60108e12b008ee22b4c35bdd6bda8593eb
dhl <lllee.dh@gmail.com>
2022-07-23 19:08:26 +0900

    algo C++ step2-lv11.5(9)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,29 +2,30 @@
 using namespace std;
 
 int main(){
-    int a, b, c;
-    int arr[3][3] = {
-        {3, 1, 6},
-        {7, 8, 4},
-        {9, 2, 3}
-    };
-
-    cin >> a >> b >> c;
-
-    arr[a][b] = c;
-
-    int M=0, m=9999;
-    for (int r=0; r<3; r++){
-        for (int c=0; c<3; c++){
-            if (arr[r][c] > M){
-                M = arr[r][c];
-            }else if (arr[r][c] < m){
-                m = arr[r][c];
-            }
+    int num[6], arr[2][3];
+    
+    for (int r=1; r>=0; r--){
+        for (int c=2; c>=0; c--){
+            cin >> arr[r][c];
         }
     }
 
-    cout << M+m << endl;
+    int idx = 0;
+    for (int r=0; r<2; r++){
+        for (int c=0; c<3; c++){
+            num[idx++] = arr[r][c];
+        }
+    }
+
+    int tmp=num[0];
+
+    num[0] = num[5];
+    num[5] = tmp;
+
+    for (int i=0; i<6; i++){
+        cout << num[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
