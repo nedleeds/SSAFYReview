391e05e9fd5a689c72d23f46f72c8f6e6fa742e9
dhl <lllee.dh@gmail.com>
2022-07-23 20:50:40 +0900

    algo C++ step2-lv12.5(1)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,26 +2,21 @@
 #include <string>
 using namespace std;
 
+struct NODE{
+    int x;
+    int y;
+}ta, tb;
+
 int main(){
-    int r;
-    char x, arr[5][5]={0};
+    cin >> ta.x >> tb.x;
     
-    cin >> r >> x;
+    ta.y = ta.x + 5;
+    tb.y = tb.x - 5;
 
-    for (int c=4; c>=0; c--){
-        arr[r-1][c] = x++;
-    }
-
-    for (int r=0; r<5; r++){
-        for (int c=0; c<5; c++){
-            if (arr[r][c]==0){
-                cout << '0';
-            }else{
-                cout << arr[r][c];
-            }
-        }
-        cout << endl;
-    }
+    cout << "ta.x=" << ta.x << endl; 
+    cout << "ta.y=" << ta.y << endl; 
+    cout << "tb.x=" << tb.x << endl; 
+    cout << "tb.y=" << tb.y << endl; 
 
     return 0;
 }
