61a8900422cff9fbca4449d50fd55f830e749ddc
dhl <lllee.dh@gmail.com>
2022-07-23 11:57:45 +0900

    algo C++ step2-lv10(11)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,29 +3,27 @@
 using namespace std;
 
 int main(){
-    int col;
-    int x = 1;
-    int arr[3][4]={0};
-    
-    cin >> col; 
-    
-    for (int r=2; r>=0; r--){
-        for (int c=3; c>-1; c--){
-            // cout << r << c << endl;
-            arr[r][c] = x++;
-            if (c==col){
-                arr[r][c] = 0;
-            }
+    int arr[4][4];
+
+    for (int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
+            cin >> arr[i][j];
         }
     }
-    
-    for (int i=0; i<3; i++){
-        for (int j=0; j<4; j++){
-            cout << arr[i][j] << " ";
+
+    for (int r=0; r<4; r++){
+        for (int c=0; c<4; c++){
+            if (arr[r][c]%2==0 && arr[r][c]!=0){
+                cout << "#";
+            }else if (arr[r][c]%2){
+                cout << "@";
+            }else if (arr[r][c]==0){
+                cout << "!";
+            }
+            
         }
         cout << endl;
     }
-    
 
     return 0;
 }
