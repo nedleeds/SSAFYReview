842b00057a0147b81bd7a01c9debafa8dbf87ab3
dhl <lllee.dh@gmail.com>
2022-07-23 14:08:42 +0900

    algo C++ step2-lv10.5(4)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 using namespace std;
 
-struct Fruit{
-    int size;
-    int price;
-}banana, apple;
+char vect[3][5] = {{'D', 'A', 'C', 'C', 'D'},
+                   {'S', 'D', 'F', 'A', 'E'},
+                   {'E', 'E', 'T', 'J', 'H'}};
+
+int CHECK(char *ch)
+{
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			if (vect[y][x] == *ch) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void INPUT(){
+    char a;
+    cin >> a;
+    if( CHECK(&a)){
+        cout << "있음" << endl;
+    } else{
+        cout << "없음" << endl;
+    }
+}
 
 int main(){
-    cin >> banana.size;
-    cin >> apple.size;
-
-    banana.price = banana.size*250;
-    apple.price = apple.size*500;
-
-    cout << banana.price + apple.price << "원" << endl;
+    
+    INPUT();
     
     return 0;
 }
