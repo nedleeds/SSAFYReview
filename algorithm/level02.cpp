42af29dfe247056f7a610298d1a900593432b9d4
dhl <lllee.dh@gmail.com>
2022-07-23 20:48:39 +0900

    algo C++ step2-lv12(10)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,31 +3,19 @@
 using namespace std;
 
 int main(){
-    int arr[3][3] = {0};
-    char x;
+    int r;
+    char x, arr[5][5]={0};
     
-    cin >> x;
+    cin >> r >> x;
 
-    if ('0' <= x && x <='9'){
-        int i=1;
-        for(int r=2; r>=0; r--){
-            for (int c=2; c>=r; c--){
-                arr[r][c] = i++;
-            }
-        }
-    }else if ('A' <= x && x<= 'Z'){
-        int i=1;
-        for(int r=2; r>=0; r--){
-            for (int c=0; c<=r; c++){
-                arr[r][c] = i++;
-            }
-        }
+    for (int c=4; c>=0; c--){
+        arr[r-1][c] = x++;
     }
 
-    for (int r=0; r<3; r++){
-        for (int c=0; c<3; c++){
+    for (int r=0; r<5; r++){
+        for (int c=0; c<5; c++){
             if (arr[r][c]==0){
-                cout << ' ';
+                cout << '0';
             }else{
                 cout << arr[r][c];
             }
--- algorithm/tempCodeRunnerFile.cpp
+++ algorithm/tempCodeRunnerFile.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+using namespace std;
 
-    //     cout << 0 << endl;
-    //     return 0;
-    // }
+int main(){
+    int r;
+    char x, arr[5][5]={'0'};
+    
+    cin >> r >> x;
+
+    for (int c=4; c>=0; c--){
+        arr[r-1][c] = x++;
+    }
+
+    for (int r=0; r<5; r++){
+        for (int c=0; c<5; c++){
+            cout << arr[r][c];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
