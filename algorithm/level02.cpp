7487eb10b070e870bfd3df9574f5fdaf10067d97
dhl <lllee.dh@gmail.com>
2022-07-23 20:07:07 +0900

    algo C++ step2-lv12(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,29 +2,19 @@
 #include <string>
 using namespace std;
 
+int Length(string s, char x){
+    return s.find(x);
+}
+
 int main(){
-    int x, arr[3][4]={0,};
+    string s1 = "MINQUEST";
+    char x, y, z;
 
-    cin >> x;
-
-    int y = 0;
-    for (int r=0; r<3; r++){
-       for (int c=2+y; c<4; c++){
-            arr[r][c] = x++;
-       }
-        y--;
-    }
-
-    for (int r=0; r<3; r++){
-        for (int c=0; c<4; c++){
-            if (arr[r][c] == 0){
-                cout << " ";
-            }else{
-                cout << arr[r][c];
-            }
-        }
-        cout << endl;
-    }
+    cin >> x >> y >> z;
     
+    cout << x << "="<< Length(s1, x) << endl;
+    cout << y << "="<< Length(s1, y) << endl;
+    cout << z << "="<< Length(s1, z) << endl;
+
     return 0;
 }
