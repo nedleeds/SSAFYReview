5abdc89a14bff07eeb6f5c9ad28d3568355f875d
dhl <lllee.dh@gmail.com>
2022-07-23 18:57:09 +0900

    algo C++ step2-lv11.5(7)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void process(char arr[3][3]){
+
+void Process(char *c, char arr[3][3]){
+    char t = *c - 'A' + 'a';
     for (int r=0; r<3; r++){
         for (int c=0; c<3; c++){
-            if ('a' <= arr[r][c] && arr[r][c] <= 'z'){
-                cout << char(arr[r][c] - ('a'-'A')) << " ";
-            }else if ('A' <= arr[r][c] && arr[r][c] <= 'Z'){
-                cout << char(arr[r][c] - ('A'-'a')) << " ";
-            }else if ('0' <= arr[r][c] && arr[r][c] <= '9'){
-                cout << char(arr[r][c] - '0' + '5') << " ";
+            if (arr[r][c] == t){
+                cout << "존재" << endl;
+                return ;
             }
         }
-        cout << endl;
     }
+    cout << "없음" << endl;
+    return ;
+}
+
+void Input(char arr[3][3]){
+    char c; 
+    cin >> c;
+    Process(&c, arr);
 }
 
 int main(){
+    int flag = 0;
     char arr[3][3] = {
-        {'a', 'b', 'E'},
-        {'E', '2', 'W'},
-        {'3', '2', '4'}
+        {'a', 'b', 'd'},
+        {'e', 'w', 'z'},
+        {'q', 'v', 'a'}
     };
 
-    process(arr);
+    Input(arr);
+
 
     return 0;
 }
