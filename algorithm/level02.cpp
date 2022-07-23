90b50dc286988eb372b76235072deff3c4b28e53
dhl <lllee.dh@gmail.com>
2022-07-23 14:19:17 +0900

    algo C++ step2-lv10.5(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int in[3];
-    int arr[3][4];
+    int r1, r2;
+    int arr[6][3], i=10;
 
-    cin >> in[0] >> in[1] >> in[2];
-    
-    for (int r=0; r<3; r++){
-        for (int c=0; c<4; c++){
-            arr[r][c] = in[r]+c;
+    cin >> r1 >> r2;
+
+    for (int c=0; c<3; c++){
+        for (int r=0; r<6; r++){
+            if (r1<=r && r<=r2){
+                arr[r][c] = 7;
+            }else{
+                arr[r][c] = i;
+            }
+            i++;
         }
     }
-    
-    for (int r=0; r<3; r++){
-        for (int c=0; c<4; c++){
+
+    for (int r=0; r<6; r++){
+        for (int c=0; c<3; c++){
             cout << arr[r][c] << " ";
         }
         cout << endl;
     }
+
+
 
     return 0;
 }
