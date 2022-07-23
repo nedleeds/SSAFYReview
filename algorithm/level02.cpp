28f29ad5d5ee3772500b34ef6ae397cc64687785
dhl <lllee.dh@gmail.com>
2022-07-23 21:44:14 +0900

    algo C++ step2-lv13(7)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,41 +2,27 @@
 #include <string>
 using namespace std;
 
-void input(int *x, int *y, int *z){
-    cin >> *x >> *y >> *z;
-}
-
-void process(int *x, int *y, int *z, int arr[2][6], int cnt[3]){
-    for (int r=0; r<2; r++){
-        for (int c=0; c<6; c++){
-            if (arr[r][c] == *x){
-                cnt[0]++;
-            }else if (arr[r][c] == *y){
-                cnt[1]++;
-            }else if (arr[r][c] == *z){
-                cnt[2]++;
+void find(char *c_, char arr[3][3]){
+    for (int r=0; r<3; r++){
+        for (int c=0; c<3; c++){
+            if (arr[r][c] == *c_){
+                cout << r << "," << c << endl;
             }
         }
     }
 }
 
-void output(int *x, int *y, int *z, int cnt[3]){
-    cout << *x << "=" << cnt[0] << "개" << endl;
-    cout << *y << "=" << cnt[1] << "개" << endl;
-    cout << *z << "=" << cnt[2] << "개" << endl;
-}
-
 int main(){
-    int arr[2][6] = {
-        {4, 5, 6, 1, 3, 1},
-        {2, 1, 3, 6, 3, 6}
+    char arr[3][3] = {
+        {'A', 'D', 'F'},
+        {'Q', 'W', 'E'},
+        {'Z', 'X', 'C'}
     };
 
-    int x, y, z, cnt[3]={0};
-    
-    input(&x, &y, &z);
-    process(&x, &y, &z, arr, cnt);
-    output(&x, &y, &z, cnt);
+    char c;
+    cin >> c;
+
+    find(&c, arr);
 
     return 0;
 }
