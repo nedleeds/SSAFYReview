2b2172283f358b8e0676d0f6310cedd396c152d6
dhl <lllee.dh@gmail.com>
2022-07-23 19:52:07 +0900

    algo C++ step2-lv12(4)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    int num[6], arr[2][3];
+    int x;
+
+    cin >> x;
+
+    for (int r=0; r<4; r++){
+        for (int c=0; c<4; c++){
+            cout << x ;
+        }
+        cout << endl;
+        x--;
+    }
     
-    for (int r=1; r>=0; r--){
-        for (int c=2; c>=0; c--){
-            cin >> arr[r][c];
-        }
-    }
-
-    int idx = 0;
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            num[idx++] = arr[r][c];
-        }
-    }
-
-    int tmp=num[0];
-
-    num[0] = num[5];
-    num[5] = tmp;
-
-    for (int i=0; i<6; i++){
-        cout << num[i] << " ";
-    }
-    cout << endl;
-
     return 0;
 }
