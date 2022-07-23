d7a7d8b7937776b0be3913e79a110bf732cb65e5
dhl <lllee.dh@gmail.com>
2022-07-23 21:09:25 +0900

    algo C++ step2-lv12.5(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    int r, c; 
-    int arr[5][5] = {
-        {4, 5, 4, 5, 4},
-        {8, 9, 8, 9, 8},
-        {1, 2, 1, 2, 1},
-        {4, 5, 4, 5, 4},
-        {6, 7, 6, 7, 6}
-    };
+    string s;
 
-    for (int i=0; i<5; i++){
-        cin >> r >> c;
-        arr[r][c]++;
-        arr[r][c] = (arr[r][c] % 10);
-    }
+    cin >> s;
+    int l = s.length();
+    cout << l << endl;
 
-    for (int r=0; r<5; r++){
-        for (int c=0; c<5; c++){
-            cout << arr[r][c];
+    int cnt=0;
+    for (int i=0; i<l; i++){
+        if(s[i]==s[l-1]){
+            cnt++;
         }
-        cout << endl;
     }
-
+    cout << cnt << endl;
 
     return 0;
 }
