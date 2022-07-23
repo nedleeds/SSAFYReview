051c4edfef4d63db1a4e956fe3aff1a5281d84ba
dhl <lllee.dh@gmail.com>
2022-07-23 17:14:20 +0900

    algo C++ step2-lv11.5(2)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    char a, b, arr[6]={'D', 'F', 'G', 'D', 'A', 'Q'};
+int arr[3][3] = {{1, 1, 1},
+                 {1, 2, 1}, 
+                 {3, 6, 3}};
 
-    cin >> a >> b;
-
-    for (int i=0; i<6; i++){
-        if (a <= arr[i] && arr[i] <= b){
-            cout << "발견!!!" << endl;
-            return 0;
+int Count(int *n){
+    int cnt=0;
+    for (int r=0; r<3; r++){
+        for (int c=0; c<3; c++){
+            if (arr[r][c] == *n){
+                cnt++;
+            }
         }
     }
+    return cnt;
+}
 
-    cout << "미발견!!!" << endl;
+int main(){
+    int num;
 
+    cin >> num;
+
+    cout << Count(&num) << endl;;
 
     return 0;
 }
