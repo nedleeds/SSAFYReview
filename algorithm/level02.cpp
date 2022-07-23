bda26bffce81442383ee27f9e8a337ab1cdf47b3
dhl <lllee.dh@gmail.com>
2022-07-23 17:11:01 +0900

    algo C++ step2-lv11.5(1)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int KFC(int target, int arr[2][5]){
-    for (int r=0; r<2; r++){
-        for (int c=0; c<5; c++){
-            if (arr[r][c] == target){
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
 int main(){
-    int arr[4][4] = {{1, 3, 6, 2},
-                     {4, 2, 4, 5},
-                     {6, 3, 7, 3},
-                     {1, 5, 4, 6}};
-    int n, select[16], s_i=0;
+    char a, b, arr[6]={'D', 'F', 'G', 'D', 'A', 'Q'};
 
-    cin >> n;
-    
-    for (int r=0; r<4; r++){
-        for (int c=0; c<4; c++){
-            if (arr[r][c] > n){
-                select[s_i++] = arr[r][c];
-            }
+    cin >> a >> b;
+
+    for (int i=0; i<6; i++){
+        if (a <= arr[i] && arr[i] <= b){
+            cout << "발견!!!" << endl;
         }
     }
 
-    for (int i=0; i<s_i; i++){
-        cout << select[i] << " ";
-    }
-    cout << endl;
+    cout << "미발견!!!" << endl;
+
 
     return 0;
 }
