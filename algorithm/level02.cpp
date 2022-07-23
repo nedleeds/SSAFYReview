61f50b99132eff5a125442fd0222ec846fbfaedb
dhl <lllee.dh@gmail.com>
2022-07-23 17:30:33 +0900

    algo C++ step2-lv11.5(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[2][3];
-
-    for (int r=0; r<2; r++){
+void process(char arr[3][3]){
+    for (int r=0; r<3; r++){
         for (int c=0; c<3; c++){
-            cin >> arr[r][c];
-        }
-    }
-
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            if (arr[r][c] == 0){
-                cout << '#';
-            }else{
-                cout << arr[r][c];
+            if ('a' <= arr[r][c] && arr[r][c] <= 'z'){
+                cout << char(arr[r][c] - ('a'-'A')) << " ";
+            }else if ('A' <= arr[r][c] && arr[r][c] <= 'Z'){
+                cout << char(arr[r][c] - ('A'-'a')) << " ";
+            }else if ('0' <= arr[r][c] && arr[r][c] <= '9'){
+                cout << char(arr[r][c] - '0' + '5') << " ";
             }
         }
         cout << endl;
     }
+}
+
+int main(){
+    char arr[3][3] = {
+        {'a', 'b', 'E'},
+        {'E', '2', 'W'},
+        {'3', '2', '4'}
+    };
+
+    process(arr);
 
     return 0;
 }
