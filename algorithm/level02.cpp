88f11b3dea3bef39572110571e782fe13c073d9d
dhl <lllee.dh@gmail.com>
2022-07-23 19:00:27 +0900

    algo C++ step2-lv11.5(8)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    int a, b, c;
+    int arr[3][3] = {
+        {3, 1, 6},
+        {7, 8, 4},
+        {9, 2, 3}
+    };
 
-void Process(char *c, char arr[3][3]){
-    char t = *c - 'A' + 'a';
+    cin >> a >> b >> c;
+
+    arr[a][b] = c;
+
+    int M=0, m=9999;
     for (int r=0; r<3; r++){
         for (int c=0; c<3; c++){
-            if (arr[r][c] == t){
-                cout << "존재" << endl;
-                return ;
+            if (arr[r][c] > M){
+                M = arr[r][c];
+            }else if (arr[r][c] < m){
+                m = arr[r][c];
             }
         }
     }
-    cout << "없음" << endl;
-    return ;
-}
 
-void Input(char arr[3][3]){
-    char c; 
-    cin >> c;
-    Process(&c, arr);
-}
-
-int main(){
-    int flag = 0;
-    char arr[3][3] = {
-        {'a', 'b', 'd'},
-        {'e', 'w', 'z'},
-        {'q', 'v', 'a'}
-    };
-
-    Input(arr);
-
+    cout << M+m << endl;
 
     return 0;
 }
