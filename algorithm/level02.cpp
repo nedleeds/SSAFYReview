852780a880d513b4010d9a34e949152f97a23878
dhl <lllee.dh@gmail.com>
2022-07-23 20:17:20 +0900

    algo C++ step2-lv12(7)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -3,18 +3,29 @@
 using namespace std;
 
 int Length(string s, char x){
-    return s.find(x);
+    int cnt = 0;
+    for (int i=0; i<s.length(); i++){
+        if (s[i]==x){
+            cnt++;
+        }
+    }
+    return cnt;
 }
 
 int main(){
-    string s1 = "MINQUEST";
+    string s;
     char x, y, z;
 
+    cin >> s;
+    if (s=="0"){
+        cout << 0 << endl;
+        return 0;
+    }
     cin >> x >> y >> z;
-    
-    cout << x << "="<< Length(s1, x) << endl;
-    cout << y << "="<< Length(s1, y) << endl;
-    cout << z << "="<< Length(s1, z) << endl;
+
+    cout << x << "="<< Length(s, x) << endl;
+    cout << y << "="<< Length(s, y) << endl;
+    cout << z << "="<< Length(s, z) << endl;
 
     return 0;
 }
