d3738c6575adb660dbae60d1ce633fed5da398b6
dhl <lllee.dh@gmail.com>
2022-07-24 14:04:39 +0900

    algo C++ step2-lv15(14)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -20,21 +20,29 @@ void sort(string *s){
 }
 
 int main(){
-    string s1 = "DATAW";
-    string s2 = "BBQK";
-    int n;
+    string s1 = "POTIO";
+    string s2 = "ABCDE";
+    string s3 = "YOURE";
+    char ans[15];
 
-    cin >> n;
+    int a, b;
+    cin >> a >> b;
 
-    if (n%2){
-        sort(&s1);
-    }else{
-        sort(&s2);
+    int k = 0;
+    for (int i=a; i<=b; i++){
+        ans[k++] = s1[i];
     }
-    
+    for (int i=a; i<=b; i++){
+        ans[k++] = s2[i];
+    }
+    for (int i=a; i<=b; i++){
+        ans[k++] = s3[i];
+    }
 
-    cout << s1 << endl;
-    cout << s2 << endl;
+    for (int x=0; x<k; x++){
+        cout << ans[x];
+    }
+    cout << endl;
 
     return 0;
 }
