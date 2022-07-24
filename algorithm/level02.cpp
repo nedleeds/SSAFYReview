b02b1e3b90613193aaf71897ec41e759fa33b12a
dhl <lllee.dh@gmail.com>
2022-07-24 13:18:23 +0900

    algo C++ step2-lv15(6)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,37 +2,28 @@
 #include <string>
 using namespace std;
 
-void swap(int *a, int *b){
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
+int main(){
+    string s;
+    cin >> s;
 
-void sort(int l[4]){
-    for (int i=0; i<3; i++){
-        for (int j=i+1; j<4; j++){
-            if (l[i] > l[j]){
-                swap(&l[i], &l[j]);
+    for (int i=0; i<s.length(); i++){
+        if (i%2==0){
+            if ('A' <= s[i] && s[i] <= 'Z'){
+                ;
+            }else{
+                cout << "일반문장" << endl;
+                return 0;
+            }
+        } else {
+            if('a' <= s[i] && s[i] <= 'z'){
+                ;
+            }else{
+                cout << "일반문장" << endl;
+                return 0;
             }
         }
     }
-}
-
-int main(){
-    string s[4];
-    int l[4];
-
-    for (int i=0; i<4; i++){
-        cin >> s[i];
-        l[i] = s[i].length();
-    }
-
-    sort(l);
-
-    for (int j=0; j<4; j++){
-        cout << l[j] << " ";
-    }
-    cout << endl;
+    cout << "개구리문장" << endl;
 
     return 0;
 }
