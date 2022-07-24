53985c82e3e47ef519078dc59fee60447f1003b7
dhl <lllee.dh@gmail.com>
2022-07-24 12:10:40 +0900

    algo C++ step2-lv14.5(4)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    int a;
-    cin >> a;
+struct Mart{
+    int st, ml, wt;
+};
 
-    int d = 0;
-    while (d < 3){
-        int c = 0;
-        while (c<5){
-            cout << a;
-            c++;
-        }
-        cout << endl;
-        d++;
-    }
-    
+int main(){
+    char mart;
+
+    Mart A = {300, 500, 1000};
+    Mart B = {450, 450, 900};
+    Mart C = {200, 150, 700};
+
+    cin >> mart;
+
+    if (mart == 'A') cout << int((A.st + A.ml + A.wt)/3) << endl;
+    else if (mart == 'B') cout << int((B.st + B.ml + B.wt)/3) << endl;
+    else if (mart == 'C') cout << int((C.st + C.ml + C.wt)/3) << endl;
 
     return 0;
 }
