d29572f3a937146bfa09b3929fb4fe5eb6bc7fbe
dhl <lllee.dh@gmail.com>
2022-07-24 14:47:26 +0900

    algo C++ step2-훈련반(10)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,54 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b){
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void sort(int arr[2][3]){
-    int n[6];
-
-    int idx = 0;
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            n[idx++] = arr[r][c];
-        }
-    }
-
-    for (int i=0; i<5; i++){
-        for (int j=i+1; j<6; j++){
-            if (n[i] > n[j]){
-                swap(&(n[i]), &(n[j]));
-            }
-        }
-    }
-
-    idx = 0;
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            arr[r][c] = n[idx++];
-        }
-    }
-}
-
 int main(){
-    int arr[2][3];
+    char arr[3][5] = {
+        {'F', 'R', 'Q', 'W', 'T'},
+        {'G', 'A', 'S', 'Y', 'Q'},
+        {'A', 'S', 'A', 'D', 'B'},
+    };
 
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            cin >> arr[r][c];
-        }
-    }
-    sort(arr);
+    int n;
+    cin >> n;
 
-    for (int r=0; r<2; r++){
-        for (int c=0; c<3; c++){
-            cout << arr[r][c] << " ";
-        }
-        cout << endl;
+    for (int r=0; r<3; r++){
+        cout << arr[r][n];
     }
+    cout << endl;
 
     return 0;
 }
