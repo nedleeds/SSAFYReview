3dd574d3d0dd60600289c08100c4e73c1b5a4b4b
dhl <lllee.dh@gmail.com>
2022-07-24 11:54:12 +0900

    algo C++ step2-lv14.5(2)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void swap(char *a, char *b){
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
 int main(){
-    char a, b;
-    char *p_a = &a; 
-    char *p_b = &b;
+    int shrimp[5][5];
+    int triangle;
+    cin >> triangle;
 
-    cin >> a >> b;
-    swap(&a, &b);
+    for (int r=0; r<5; r++){
+        for (int c=0; c<5; c++){
+            cin >> shrimp[r][c];
+        }
+    }
 
-    cout << a << " " << b << endl;
+    if (triangle == 1){
+        for (int r=0; r<5; r++){
+            for (int c=0; c<=r; c++){
+                cout << shrimp[r][c] << " ";
+            }
+            cout << endl;
+        }
+    }else if(triangle==2){
+        for (int r=0; r<5; r++){
+            for (int c=0; c<5-r; c++){
+                cout << shrimp[r][c] << " ";
+            }
+            cout << endl;
+        }
+    } 
 
     return 0;
 }
