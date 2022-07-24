16dc5f7838418c2465f726f120485e9f44d528d2
dhl <lllee.dh@gmail.com>
2022-07-24 13:50:49 +0900

    algo C++ step2-lv15(12)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -7,31 +7,14 @@ void CountLine(string *s){
 }
 
 int main(){
-    string s[4];
+    string s[2], s2;
 
-    int cnt_A = 0;
-    int cnt_B = 0;
-    for (int i=0; i<4; i++){
+    for (int i=0; i<2; i++){
         cin >> s[i];
-        for (int j=0; j<s[i].length(); j++){
-            if (s[i][j] == 'A'){
-                cnt_A ++;
-            }else if (s[i][j] == 'B'){
-                cnt_B ++;
-            }
-        }
+        s2.append(s[i]);
     }
 
-    if (cnt_A*cnt_B>0 && cnt_A+cnt_B>1){
-        cout << "대발견" << endl;
-    }
-    if (cnt_A*cnt_B==0 && (cnt_A>0 || cnt_B>0)){
-        cout << "중발견" << endl;
-    }
-    if (cnt_A == 0 && cnt_B == 0){
-        cout << "미발견" << endl;
-    }
-    
+    cout << s2 << endl;
 
     return 0;
 }
