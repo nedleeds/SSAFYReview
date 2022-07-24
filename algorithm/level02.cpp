09b93f66c19d83525f61a9f1daf4bc60d9dc7702
dhl <lllee.dh@gmail.com>
2022-07-24 11:56:06 +0900

    algo C++ step2-lv14.5(3)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,31 +2,20 @@
 using namespace std;
 
 int main(){
-    int shrimp[5][5];
-    int triangle;
-    cin >> triangle;
+    int a;
+    cin >> a;
 
-    for (int r=0; r<5; r++){
-        for (int c=0; c<5; c++){
-            cin >> shrimp[r][c];
+    int d = 0;
+    while (d < 3){
+        int c = 0;
+        while (c<5){
+            cout << a;
+            c++;
         }
+        cout << endl;
+        d++;
     }
-
-    if (triangle == 1){
-        for (int r=0; r<5; r++){
-            for (int c=0; c<=r; c++){
-                cout << shrimp[r][c] << " ";
-            }
-            cout << endl;
-        }
-    }else if(triangle==2){
-        for (int r=0; r<5; r++){
-            for (int c=0; c<5-r; c++){
-                cout << shrimp[r][c] << " ";
-            }
-            cout << endl;
-        }
-    } 
+    
 
     return 0;
 }
