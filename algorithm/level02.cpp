dd24dab1f8ebb05552c1322601557eb9cc473c9a
dhl <lllee.dh@gmail.com>
2022-07-24 13:58:45 +0900

    algo C++ step2-lv15(13)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,18 +2,38 @@
 #include <string>
 using namespace std;
 
-void CountLine(string *s){
-    cout << (*s).length() << "=" << *s << endl;
+void swap(char *s, char *t){
+    char tmp = *s;
+    *s = *t;
+    *t = tmp;
+}
+
+void sort(string *s){
+    int l = (*s).length();
+    for (int i=0; i<l-1; i++){
+        for (int j=i+1; j<l; j++){
+            if ((*s)[i] > (*s)[j]){
+                swap(&(*s)[i], &(*s)[j]);
+            }
+        }
+    }
 }
 
 int main(){
-    string s[2], s2;
+    string s1 = "DATAW";
+    string s2 = "BBQK";
+    int n;
 
-    for (int i=0; i<2; i++){
-        cin >> s[i];
-        s2.append(s[i]);
+    cin >> n;
+
+    if (n%2){
+        sort(&s1);
+    }else{
+        sort(&s2);
     }
+    
 
+    cout << s1 << endl;
     cout << s2 << endl;
 
     return 0;
