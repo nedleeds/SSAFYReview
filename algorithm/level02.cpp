103802cb866d78e0780737c5d2226f1639454203
dhl <lllee.dh@gmail.com>
2022-07-24 11:25:32 +0900

    algo C++ step2-lv14.5(1)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -9,24 +9,14 @@ void swap(char *a, char *b){
 }
 
 int main(){
-    int arr[6] = {10, 50, 40, 20, 30, 40};
-    int arr2[6] = {0}, cnt[6] = {0};
-    
-    for (int i=0; i<6; i++){
-        cin >> arr2[i];
-    }
+    char a, b;
+    char *p_a = &a; 
+    char *p_b = &b;
 
-    for (int j=0; j<6; j++){
-        for (int i=0; i<6; i++){
-            if (arr2[j] < arr[i]){
-                cnt[j]++;
-            }
-        }
-    }
+    cin >> a >> b;
+    swap(&a, &b);
 
-    for (int i=0; i<6; i++){
-        cout << arr2[i] << "=" << cnt[i] << "개" << endl;
-    }
+    cout << a << " " << b << endl;
 
     return 0;
 }
