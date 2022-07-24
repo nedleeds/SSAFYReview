d03b9e4d6ef56a8765ef2a8fdedf66c2958c239f
dhl <lllee.dh@gmail.com>
2022-07-24 12:47:39 +0900

    algo C++ step2-lv15(1)

--- algorithm/level02.cpp
+++ algorithm/level02.cpp
@@ -2,33 +2,16 @@
 #include <string>
 using namespace std;
 
-void Output(int arr[3][3]){
-    for (int r=0; r<3; r++){
-        for (int c=0; c<3; c++){
-            if (arr[r][c] == 0){
-                cout << " ";
-            }else{
-                cout << arr[r][c];
-            }
-        }
-        cout << endl;
-    }
-}
-
-void Magic(int arr[3][3]){
-    int i = 1;
-    for (int r=0; r<3; r++){
-        for (int c=r; c<3; c++){
-            arr[r][c] = i++;
-        }
-    }
-}
-
 int main(){
-    int arr[3][3] = {0};
+    string s1, s2; 
 
-    Magic(arr);
-    Output(arr);
+    cin >> s1 >> s2;
+    
+    if (s1 == s2){
+        cout << "같음" << endl;
+    }else{
+        cout << "다름" << endl;
+    }
 
     return 0;
 }
