f3306713b5977f6669e923a071d7e48e4b45325d
dhl <lllee.dh@gmail.com>
2022-07-25 18:32:30 +0900

    algorithm problem lv18-(3)

--- algorithm/week1/day1/promin.cpp
+++ algorithm/week1/day1/promin.cpp
@@ -6,17 +6,22 @@ int main(){
     cin.tie();
     cout.tie();
 
+    int arr[3][5] = {
+        {1, 3, 3, 5, 1},
+        {3, 6, 2, 4, 2},
+        {1, 9, 2, 6, 5},
+    };
     int DAT[10]={0};
+    int n;
+    cin >> n;
     for (int r=0; r<3; r++){
-        for (int c=0; c<3; c++){
-            int tmp;
-            cin >> tmp;
-            DAT[tmp]++;
+        for (int c=0; c<5; c++){
+            DAT[arr[r][c]]++;
         }
     }
 
     for (int j=1; j<=9; j++){
-        if (DAT[j]==0){
+        if (DAT[j]==n){
             cout << j << " ";
         }
     }
