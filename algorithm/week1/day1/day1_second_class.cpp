8645aa9fd9a2d33c360296ae2c3ae73b0a5bd98a
dhl <lllee.dh@gmail.com>
2022-07-25 15:32:15 +0900

    algorithm afternoon class2

--- algorithm/week1/day1/day1_second_class.cpp
+++ algorithm/week1/day1/day1_second_class.cpp
@@ -6,12 +6,64 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie();
     cout.tie();
+    //문제 1
+    // int arr[10];
+    // int cnt[2] = {0};
 
-    int a = 1;
-    for (int i = 0; i < 10; i++)
-    {
-        a = a * 2;
-        cout << a << "\n";
+    // for (int i = 0; i < 10; i++){
+    //     cin >> arr[i];
+    //     cnt[arr[i]]++;
+    // }
+
+    // cout << "0:" << cnt[0] << "\n";
+    // cout << "1:" << cnt[1] << "\n";
+
+    //문제 2
+    // int cnt[10]={0};
+    // for (int i=0; i<10; i++){
+    //     int temp;
+    //     cin >> temp;
+    //     cnt[temp]++;
+    // }
+
+    // for (int i=0; i<10; i++){
+    //     cout << i << ": " << cnt[i] << "\n";
+    // }
+
+    //문제 3
+    // int arr[10] = {0};
+    // for (int i=0; i<10; i++){
+    //     int tmp;
+    //     cin >> tmp;
+    //     arr[tmp] = i;
+    // }
+
+    // for (int j=0; j<10; j++){
+    //     cout << j << ":" << arr[j] << "\n";
+    // }
+
+    int MAP[10][10]{
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 5, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 10, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 10, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int row, col;
+    cin >> row >> col;
+    if (MAP[row][col]){
+        for (int i=0; i<MAP[row][col]; i++){
+            cout << "BOOM!";
+        }
+        cout << "\n";
+    }else{
+        cout << "STILL ALIVE!\n";
     }
 
     return 0;
