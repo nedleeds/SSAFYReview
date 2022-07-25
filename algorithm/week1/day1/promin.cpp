490166173990dd9ba0b01a4e4bf812a6a1510af2
dhl <lllee.dh@gmail.com>
2022-07-25 18:28:28 +0900

    algorithm problem lv18-(2)

--- algorithm/week1/day1/promin.cpp
+++ algorithm/week1/day1/promin.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int MAP[3][4] = {
-    {65000, 35, 42, 70},
-    {70, 35, 65000, 1300},
-    {65000, 30000, 38, 42}
-};
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie();
     cout.tie();
 
-    int DAT[65535]={0};
+    int DAT[10]={0};
     for (int r=0; r<3; r++){
-        for (int c=0; c<4; c++){
-            DAT[MAP[r][c]]++;
+        for (int c=0; c<3; c++){
+            int tmp;
+            cin >> tmp;
+            DAT[tmp]++;
         }
     }
 
-    int M=0, id=0;
-    for (int i=0; i<65535; i++){
-        if (M < DAT[i]){
-            M = DAT[i];
-            id = i;
+    for (int j=1; j<=9; j++){
+        if (DAT[j]==0){
+            cout << j << " ";
         }
     }
-    cout << id << "\n";
+    cout << "\n";
     
     return 0;
 } 
