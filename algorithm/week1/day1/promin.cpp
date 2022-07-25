5c97c3082649ec4baf8f2a0319e1ce858480cf9f
dhl <lllee.dh@gmail.com>
2022-07-25 20:41:34 +0900

    algorithm problem lv19-(3)

--- algorithm/week1/day1/promin.cpp
+++ algorithm/week1/day1/promin.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int MAP[5][5] = {
-    {3, 3, 5, 3, 1},
-    {2, 2, 4, 2, 6},
-    {4, 9, 2, 3, 4},
-    {1, 1, 1, 1, 1},
-    {3, 3, 5, 9, 2}
+char MAP[4][5] = {
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'}
 };
 
-int dir[4][2] = {
+int dir[8][2] = {
     {-1,-1},
+    {-1, 0},
     {-1, 1},
+    { 0, 1},
+    { 1, 1},
+    { 1, 0},
     { 1,-1},
-    { 1, 1}
+    { 0,-1},
 };
 
-int sum(int r, int c){
-    int s = 0;
-    for (int i = 0; i<4; i++){
+void explode(int r, int c){
+    for (int i = 0; i<8; i++){
         int nr = r + dir[i][0];
         int nc = c + dir[i][1];
 
-        if (0 <= nr && nr <=4 && 0 <= nc && nc <= 4){
-            s += MAP[nr][nc];
+        if (0 <= nr && nr <=3 && 0 <= nc && nc <= 4){
+            MAP[nr][nc] = '#';
         }
     }
-    return s;
 }
 
 int main()
@@ -35,19 +36,18 @@ int main()
     cin.tie();
     cout.tie();
 
-    int s = 0, M=0, M_r, M_c;
-    for (int r=0; r<5; r++){
-        for (int c=0; c<5; c++){
-            s = sum(r, c);
-            if (s > M){
-                M_r = r;
-                M_c = c;
-                M = s;
-            }
-        }
+    for (int i=0; i<2; i++){
+        int r, c;
+        cin >> r >> c;
+        explode(r, c);
     }
-    
-    cout << M_r << " " << M_c << "\n";
+
+    for (int r=0; r<4; r++){
+        for (int c=0; c<5; c++){
+            cout << MAP[r][c] << " ";
+        }
+        cout << "\n";
+    }
 
     return 0;
 } 
--- algorithm/week1/day1/tempCodeRunnerFile.cpp
+++ algorithm/week1/day1/tempCodeRunnerFile.cpp
@@ -1,8 +1,53 @@
+#include <iostream>
+using namespace std;
 
-    // for (int j=0; j<26; j++){
-    //     if (M < DAT[j]){
-    //         M = j;
-    //     }
-    // }
-    // cout << M << "\n";
-    // cout << char(DAT[
+char MAP[4][5] = {
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'},
+    {'_', '_', '_', '_', '_'}
+};
+
+int dir[8][2] = {
+    {-1,-1},
+    {-1, 0},
+    {-1, 1},
+    { 0, 1},
+    { 1, 1},
+    { 1, 0},
+    { 1,-1},
+    { 0,-1},
+};
+
+void explode(int r, int c){
+    for (int i = 0; i<8; i++){
+        int nr = r + dir[i][0];
+        int nc = c + dir[i][1];
+
+        if (0 <= nr && nr <=3 && 0 <= nc && nc <= 4){
+            MAP[nr][nc] = '#';
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    for (int i=0; i<2; i++){
+        int r, c;
+        cin >> r >> c;
+        explode(r, c);
+    }
+
+    for (int r=0; r<4; r++){
+        for (int c=0; c<5; c++){
+            cout << MAP[r][c] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+} 
