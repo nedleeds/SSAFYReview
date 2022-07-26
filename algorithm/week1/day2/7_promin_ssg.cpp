e31554e9cd09e824ca30f78cacc4eaee27ef8752
nedleeds <lllee.dh@gmail.com>
2022-07-26 13:43:21 +0900

    promin level-21(2)

--- algorithm/week1/day2/7_promin_ssg.cpp
+++ algorithm/week1/day2/7_promin_ssg.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// path -> DAT
-// index : 층 번호
-// value : 내가 현재 층에서 다음 층으로 내려가기 위해 "선택한 경로"
+string id = "qlqlaqkq";
+string passwd = "tkaruqtkf";
 
-
-// level : 층 -> 어느 층까지 도달해야 종료되는가? 
-// 주사위를 3번 던지니깐 3층까지.
-void func(int level) {
-
-    // 기저 조건 
-    if (level == 2) {
-        return ;
-    }
-
-    // 재귀 구성
-    for (int i = 0; i < 3; i++) {
-        func(level + 1);
-    }
-}
 
 int main() {
-    func(0);
+    string i, p;
+    cin >> i;
+    cin >> p;
+
+    if (i == id && p == passwd) {
+        cout << "LOGIN\n";
+    }
+    else {
+        cout << "INVALID\n";
+    }
+
     return 0;
 }
