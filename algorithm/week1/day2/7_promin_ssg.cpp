d7097242c3cb697adda49fe31851789768ada209
nedleeds <lllee.dh@gmail.com>
2022-07-26 14:03:22 +0900

    promin level-21(5)

--- algorithm/week1/day2/7_promin_ssg.cpp
+++ algorithm/week1/day2/7_promin_ssg.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int lvl;
-int path[100] = {0};
-
-void f(int level) {
-    
-    cout << level;
-    if (level == lvl) {
-        return ;
-    }
-
-    for (int i = 0; i < 2; i++) {
-        f(level + 1);
-    }
-}
+string s[3];
 
 int main() {
-    cin >> lvl ;
+    
+    int M = 0, M_idx;
 
-    f(0);
+    for (int i = 0; i < 3; i++) {
+        cin >> s[i];
+        if (s[i].length() > M) {
+            M = s[i].length();
+            M_idx = i;
+        }
+    }
+
+    string tmp;
+    tmp = s[0];
+    s[0] = s[M_idx];
+    s[M_idx] = tmp;
+
+    for (int i = 0; i < 3; i++) {
+        cout << s[i] << "\n";
+    }
 
     return 0;
 }
