218290a3ce8de623a444a7b30a20f49cb099b9ee
nedleeds <lllee.dh@gmail.com>
2022-07-26 10:59:42 +0900

    promin level-20(5)

--- algorithm/week1/day2/4_promin_recursion.cpp
+++ algorithm/week1/day2/4_promin_recursion.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int arr[6] = {0};
-
-void jump(int num, int jumpCnt) {
-	if (jumpCnt == 3) {
-		cout << num << " ";
+char arr[5];
+void printing(int idx) {
+	if (idx == 5) {
+		cout << "\n";
 		return;
 	}
-	
-	jump(num + 2, jumpCnt + 1);
-	cout << num << " ";
+
+	cout << arr[idx];
+	printing(idx + 1);
+	cout << arr[idx];
 }
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 	
-	int num = 0;
-	cin >> num;
-
-	jump(num, 0);
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+	
+	
+	printing(0);
 	cout << "\n";
 
 	return 0;
