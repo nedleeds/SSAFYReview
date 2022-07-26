7bcbaf50a435acf371dbc8f5c0e0b5f8bf85977a
nedleeds <lllee.dh@gmail.com>
2022-07-26 16:03:59 +0900

    promin level-23(2)

--- algorithm/week1/day2/7_promin_ssg.cpp
+++ algorithm/week1/day2/7_promin_ssg.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 using namespace std;
 
-string name;
-string target;
+string alphabets;
+string check[2] = {
+	"BT", "TB"
+};
 string path;
-int DAT[26];
-
+int total = 0;
+int cnt = 0;
 void func(int level) {
 	// 기저 조건
-	if (level == 3) {
-		cout << path << "\n";
+	if (level == 4) {
+		total++;
+		for (int i = 0; i < 2; i++) {
+			int find_idx = path.find(check[i]);
+			if (0 <= find_idx && find_idx < 3) {
+				cnt++;
+				return;
+			}
+		}
 		return;
 	}
 
 	// 재귀 조건
-	for (int i = 0; i < name.length(); i++) {
-		if (DAT[name[i]-'A'] == 1) {
-			continue;
-		}
-		path.append(1, name[i]);
-		DAT[name[i] - 'A'] = 1;
+	for (int i = 0; i < alphabets.length(); i++) {
+		path.append(1, alphabets[i]);
 		func(level + 1);
 		path.pop_back();
-		DAT[name[i] - 'A'] = 0;
 	}
 }
 
@@ -31,9 +35,11 @@ int main() {
 	cin.tie();
 	cout.tie();
 
-	cin >> name;
+	cin >> alphabets;
 
 	func(0);
+
+	cout << total-cnt << "\n";
 
 	return 0;
 }
