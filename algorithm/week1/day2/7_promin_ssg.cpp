069700743ff7b7037cd2ab3e9c4e06e4f9a46968
nedleeds <lllee.dh@gmail.com>
2022-07-26 13:40:18 +0900

    promin level-21(1)

--- algorithm/week1/day2/7_promin_ssg.cpp
+++ algorithm/week1/day2/7_promin_ssg.cpp
@@ -6,28 +6,22 @@ using namespace std;
 // value : 내가 현재 층에서 다음 층으로 내려가기 위해 "선택한 경로"
 
 
-int path[10];
-int branch, lvl;
-int cnt = 0;
-
 // level : 층 -> 어느 층까지 도달해야 종료되는가? 
 // 주사위를 3번 던지니깐 3층까지.
 void func(int level) {
-    cnt++;
 
     // 기저 조건 
-    if (level == lvl) {
+    if (level == 2) {
         return ;
     }
 
     // 재귀 구성
-    for (int i = 0; i < branch; i++) {
+    for (int i = 0; i < 3; i++) {
         func(level + 1);
     }
 }
 
 int main() {
-    cin >> branch >> lvl;
     func(0);
-    cout << cnt << "\n";
+    return 0;
 }
