6f8c588376b78655fcca46cb24af0ec58d9f0d7b
nedleeds <lllee.dh@gmail.com>
2022-07-26 11:38:37 +0900

    promin level-20.5(4)

--- algorithm/week1/day2/4_promin_recursion.cpp
+++ algorithm/week1/day2/4_promin_recursion.cpp
@@ -1,45 +1,34 @@
 #include <iostream>
 using namespace std;
 
-string arr;
+int arr1[4][4];
+int arr2[4][4];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 
-	cin >> arr;
-
-	int l = arr.length();
-	char a2[5] = {};
-	char a3[5] = {};
-
-	int a2_len=0, a3_len=0;
-	for (int i = 0; i < l / 2; i++) {
-		a2[i] = arr[i];
-		a2_len++;
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			cin >> arr1[r][c];
+		}
 	}
 
-	int k = 0;
-	for (int j = l/2; j < l; j++) {
-		a3[k++] = arr[j];
-		a3_len++;
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			cin >> arr2[r][c];
+		}
 	}
 
-	if (a2_len != a3_len) {
-		cout << "다른문장\n";
-	}
-	else {
-		for (int j = 0; j < a2_len; j++) {
-			if (a2[j] != a3[j]) {
-				cout << "다른문장\n";
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			if (arr1[r][c] == 1 && arr2[r][c] == 1) {
+				cout << "걸리다\n";
 				return 0;
 			}
 		}
-		cout << "동일한문장\n";
 	}
-
-	
-
+	cout << "걸리지않는다\n";
 	return 0;
 }
