f8e58983f2a4aa2ffa005f54ad9d15c9d36d6707
nedleeds <lllee.dh@gmail.com>
2022-07-26 11:24:22 +0900

    promin level-20.5(2)

--- algorithm/week1/day2/4_promin_recursion.cpp
+++ algorithm/week1/day2/4_promin_recursion.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void bbq(int num) {
-	if (num == 3) {
-		return;
-	}
-
-	bbq(num+1);
-}
-
+string arr;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
-		
-	bbq(0);
-	cout << "\n";
+
+	cin >> arr;
+
+	int l = arr.length();
+	for (int start = l - 1; start >= 0; start--) {
+		for (int j = start; j < l; j++) {
+			cout << arr[j];
+		}
+		cout << "\n";
+	}
 
 	return 0;
 }
