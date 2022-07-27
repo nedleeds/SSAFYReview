c85b86b6e9b55ab5f06a0657bb3ecb593b119555
nedleeds <lllee.dh@gmail.com>
2022-07-27 15:03:19 +0900

    minco lv23(3)

--- algorithm/week1/day3/09_minco_lv22.cpp
+++ algorithm/week1/day3/09_minco_lv22.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 using namespace std;
 
+
+string branch[1000];
+string path[4]; //path의 index는 level로 넣어줘야겠네.
+// index로 넣고, index로 erase하는게 더 쉽다.
+int branch_len;
+int cnt = 0;
+
+// branch - 6가지 종류의 카드 : 처음에 입력을 받음
+// level - 4층까지 쭉 넣고, 4장일 떄 CHRISTMAS랑 비교
+void func(int level) {
+	if (level == 4) {
+		// 새로운 path 를 concatenate로 만들어서 christams 랑 비교.
+		string newS;
+		for (int i = 0; i < level; i++) {
+			newS.append(path[i]);
+		}
+		if (newS == "CHRISTMAS") {
+			cnt++;
+		}
+		return;
+	}
+
+	for (int i = 0; i < branch_len; i++) {
+		// 가지치기 - 현재 path의 길이만큼 cristmas랑 비교
+		// 다르면? continue.
+		path[level] = branch[i];
+		func(level + 1);
+		path[level] = "";
+	}
+
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 
-	string name;
-	int cnt;
 
-	cin >> name;
-	cin >> cnt;
+	cin >> branch_len;
 
-	for (int i = 0; i < cnt; i++) {
-		string checklist;
-		cin >> checklist;
-
-		if (checklist.find(name) != string::npos)
-			cout << "O\n";
-		else
-			cout << "X\n";
+	for (int i = 0; i < branch_len; i++) {
+		cin >> branch[i];
 	}
 
+	func(0);
+	cout << cnt << '\n';
 
 	return 0;
 }
