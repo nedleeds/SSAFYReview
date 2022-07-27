856e10156b4b573f663a58f61e873c7e9ed8aa23
nedleeds <lllee.dh@gmail.com>
2022-07-27 20:54:33 +0900

    minco lv25(2)-article

--- algorithm/week1/day3/12_minco_lv25.cpp
+++ algorithm/week1/day3/12_minco_lv25.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 using namespace std;
 
+int checkNum(char c) {
+	if ('0' <= c && c <= '9') {
+		return 1;
+	}
+	return 0;
+}
+string ans[1000];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		string subject;
-		cin >> subject;
 
-		int j = 0;
-		while ( int start_idx = subject.find("[", j) != -1){
-			int end_idx = subject.find("]");
-			for (int i = start_idx; i <= end_idx; i++) {
-				cout << subject[i];
+	int article_num, cnt = 0;
+	cin >> article_num;
+
+	for (int i = 0; i < article_num; i++) {
+		string article;
+		cin >> article;
+
+		int find_idx = 0;
+		int start_idx;
+		while ((start_idx = article.find("[", find_idx)) != -1) {
+			int end_idx = article.find("]", find_idx);
+			for (int j = start_idx + 1; j <= end_idx - 1; j++) {
+				if (checkNum(article[j])) {
+					ans[cnt].push_back(article[j]);
+				}
 			}
-			j = end_idx + 1;
+			if (ans[cnt] != "") {
+				cnt++;
+			}
+
+			find_idx = end_idx + 1;
 			cout << '\n';
 		}
 	}
-	
 
+	for (int i = 0; ans[i] != ""; i++) {
+		if (ans[i].length() == 5) {
+			cout << '[' << ans[i] << "]\n";
+		}
+	}
 
 	return 0;
 }
