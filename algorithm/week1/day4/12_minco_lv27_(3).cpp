356be6c2a43186e42a53f3008b0360bd3baf2d37
nedleeds <lllee.dh@gmail.com>
2022-07-28 19:55:54 +0900

    minco-lv27(3)

---
+++ algorithm/week1/day4/12_minco_lv27_(3).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	int num;
+	vector<int> v;
+	cin >> num;
+
+	for (int i = 1; i <= 4; i++) {
+		v.push_back(num * i);
+	}
+
+	for (int j = 0; j < v.size(); j++) {
+		cout << v[j] << ' ';
+	}
+	cout << "\n";
+
+	return 0;
+}
