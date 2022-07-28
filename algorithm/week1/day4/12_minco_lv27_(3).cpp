d3fbe10d4ddaf9aa8e94a8a486032238f3785d28
nedleeds <lllee.dh@gmail.com>
2022-07-28 20:06:17 +0900

    minco-lv27(4)

--- algorithm/week1/day4/12_minco_lv27_(3).cpp
+++ algorithm/week1/day4/12_minco_lv27_(3).cpp
@@ -2,21 +2,22 @@
 #include <vector>
 using namespace std;
 
+vector<char> v;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 
 	int num;
-	vector<int> v;
 	cin >> num;
 
-	for (int i = 1; i <= 4; i++) {
-		v.push_back(num * i);
+	num -= 11;
+	for (int i = 0; i < 4; i++) {
+		v.push_back('A'+ num + i);
 	}
 
 	for (int j = 0; j < v.size(); j++) {
-		cout << v[j] << ' ';
+		cout << v[j];
 	}
 	cout << "\n";
 
