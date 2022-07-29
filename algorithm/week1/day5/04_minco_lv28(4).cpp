d927661bf4948f3b0fb9952badcda94809ca8957
nedleeds <lllee.dh@gmail.com>
2022-07-29 11:31:28 +0900

    minco-lv28(6)

--- algorithm/week1/day5/04_minco_lv28(4).cpp
+++ algorithm/week1/day5/04_minco_lv28(4).cpp
@@ -3,20 +3,28 @@
 using namespace std;
 
 int nodeNum;
-int adj_arr[7][7];
+int adj_arr[100][100];
 int visited[10];
+vector<int> path;
 
 void dfs(int now) {
 
-	cout << now << ' ';
+	if (path.size() == 3) {
+		for (int i = 0; i < path.size(); i++) {
+			cout << path[i] << ' ';
+		}
+		cout << '\n';
+	}
 
 	for (int next = 0; next < nodeNum; next++) {
 		if (adj_arr[now][next] == 0) continue;
 		if (visited[next] == 1) continue;
-
+		
+		path.push_back(next);
 		visited[next] = 1;
 		dfs(next);
 		visited[next] = 0;
+		path.pop_back();
 	}
 }
 
@@ -33,7 +41,11 @@ int main() {
 		}
 	}
 
+	path.push_back(0);
+	visited[0] = 1;
 	dfs(0);
+	visited[0] = 0;
+	path.pop_back();
 
 	return 0;
 }
