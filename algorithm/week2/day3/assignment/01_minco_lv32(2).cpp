2edfc2b35c72011f62a8673e774c1a2d3cb7cd9e
nedleeds <lllee.dh@gmail.com>
2022-08-03 23:22:36 +0900

    minco algo lv32(2)

--- algorithm/week2/day3/assignment/01_minco_lv32(2).cpp
+++ algorithm/week2/day3/assignment/01_minco_lv32(2).cpp
@@ -11,17 +11,24 @@ struct Student {
 
 vector<Student> students;
 void doSort(vector<Student> s) {
-	for (int i = 1; i < s.size(); i++) {
-		Student key = s[i];
-		int j = i - 1;
-		while (j >= 0 && s[j].score < key.score) {
-			s[j] = key;
-			j += 1;
+	int i, j;
+	Student key;
+	cout << s[0].name <<'\n';
+	for (i = 1; i < s.size(); i++) {
+		key = s[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (s[j].score <= key.score)
+				s[j + 1] = s[j];
+			else
+				break;
 		}
+		s[j + 1] = key;
 
-		for (int k = 0; k < i; k++)
-			cout << s[k].name << ' ';
-
+		for (int k = 0; k <= i; k++) {
+			if (k < 3) {
+				cout << s[k].name << ' ';
+			}
+		}
 		cout << '\n';
 	}
 }
@@ -35,13 +42,13 @@ int main() {
 	cin >> n;
 
 	int MAX = -2134567890;
-	
+
 	for (int i = 0; i < n; i++) {
 		Student now;
 		cin >> now.name >> now.score;
 		students.push_back(now);
 	}
-	
+
 	doSort(students);
 
 	return 0;
