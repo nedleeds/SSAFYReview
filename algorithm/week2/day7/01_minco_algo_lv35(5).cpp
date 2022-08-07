8cfbb20ec6b05e26f893a376b74cd6fc9de0b460
dhl <lllee.dh@gmail.com>
2022-08-07 17:18:32 +0900

    Add : week2 day7 algo minco-lv35(5)

---
+++ algorithm/week2/day7/01_minco_algo_lv35(5).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+    
+    int n, i, p2=0, p3=0, p5=0, minValue;
+
+    cin >> n;
+
+    vector<int> a(n);
+
+    a[0] = 1;
+
+    for(i=1; i<n; i++) {
+        minValue = min(min(a[p2]*2,a[p3]*3),a[p5]*5);
+        if (minValue == a[p2]*2) p2++;
+        if (minValue == a[p3]*3) p3++;
+        if (minValue == a[p5]*5) p5++;
+        a[i] = minValue;
+    }
+
+    cout << a[n-1];
+
+    return 0;
+}
