783d01ba8010422fb713b2ba553556b5388cdcaa
nedleeds <lllee.dh@gmail.com>
2022-08-18 13:31:35 +0900

    Add : algo greedyTop(3) not-solved

--- algorithm/week4/day3_thursday/01_greedyTp(3).cpp
+++ algorithm/week4/day3_thursday/01_greedyTp(3).cpp
@@ -30,9 +30,9 @@ int main() {
 
 	// 참석자 수 6~ :
 	// 1로 전부 채웠을 때 : minPrice1*N
-	// 6+1로 채웠을 때 : (N/minPrice6)*minPrice6 + minPrice1*(N-N/minPrice6)
+	// 6+1로 채웠을 때 : (N/minPrice6)*minPrice6 + minPrice1*(N-(N/minPrice6))
 	// 6로 전부 채웠을 때: 
-	
+
 	int minPrice1 = price1[0];
 	int minPrice6 = price6[0];
 	if (numPeople <= 6) {
@@ -42,17 +42,28 @@ int main() {
 			cout << minPrice1 * numPeople << '\n';
 	}
 	else {
+		int sum1 = 0, sum2 = 0, sum3 = 0;
 		// 1로 채웠을 때
-		int sum1 = minPrice1 * numPeople;
+		sum1 = minPrice1 * numPeople;
 		// 6, 1로 채울 때
 		while (numPeople > 0) {
-			if ()
+			/* 7명 -> 6명 + 1명
+			*  8명 -> 6명 + 2명
+			*  12명 -> 6명 + 1*6명? // 1*12 // 6*2
+			*  13명 -> 12명 1명
+			*/
+			int priceOf6 = (numPeople / 6) * minPrice6;
+			numPeople -= (numPeople / 6)*6;
+			int priceOf1 = minPrice1 * numPeople;
+			numPeople -= numPeople;
+			sum2 = priceOf1 + priceOf6;
 		}
 		// 6로 채웠을 때
-		int sum3 = (numPeople / 6 + 1) * minPrice6;
+		sum3 = (numPeople / 6 + 1) * minPrice6;
 
+		cout << min(min(sum1, sum2), sum3) << '\n';
 	}
 
-	
+
 	return 0;
 }
