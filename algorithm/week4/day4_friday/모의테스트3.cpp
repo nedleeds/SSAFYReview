2f47d89df8298ba9b3c4645407ce4016df20cce8
nedleeds <lllee.dh@gmail.com>
2022-08-19 20:21:25 +0900

    Fix : 모의테스트 3번

--- algorithm/week4/day4_friday/모의테스트3.cpp
+++ algorithm/week4/day4_friday/모의테스트3.cpp
@@ -13,118 +13,106 @@ struct Pos { int row, col; };
 vector<Pos> path;
 int checkNum;
 void dfs(Pos now) {
-	for (int i = 0; i < 4; i++) {
-		Pos next = { now.row + dr[i], now.col + dc[i] };
-		if (next.row < 0 || next.col < 0 || next.row > rowMax || next.col > 10)
-			continue;
-		if (visited[next.row][next.col] != 0)
-			continue;
-		if (arr[next.row][next.col] != checkNum)
-			continue;
-		visited[next.row][next.col] = arr[next.row][next.col];
-		dfs(next);
-	}
-	path.push_back(now);
+    for (int i = 0; i < 4; i++) {
+        Pos next = { now.row + dr[i], now.col + dc[i] };
+        if (next.row < 0 || next.col < 0 || next.row >= rowMax || next.col >= 10)
+            continue;
+        if (visited[next.row][next.col] != 0)
+            continue;
+        if (arr[next.row][next.col] != checkNum)
+            continue;
+        visited[next.row][next.col] = checkNum;
+        dfs(next);
+    }
+    path.push_back(now);
 }
 
 void initVisited() {
-	for (int r = 0; r < rowMax; r++)
-		for (int c = 0; c < 10; c++)
-			visited[r][c] = 0;
+    for (int r = 0; r < rowMax; r++)
+        for (int c = 0; c < 10; c++)
+            visited[r][c] = 0;
 }
 
 void doGravity() {
-	vector<int> nonZerosEachCol[10];
-	for (int c = 0; c < 10; c++) {
-		for (int r = rowMax - 1; r >= 0; r--) {
-			if (arr[r][c] != 0) {
-				nonZerosEachCol[c].push_back(arr[r][c]);
-				arr[r][c] = 0;
-			}
-		}
-	}
+    vector<int> nonZerosEachCol[10];
+    for (int c = 0; c < 10; c++) {
+        for (int r = rowMax - 1; r >= 0; r--) {
+            if (arr[r][c] != 0) {
+                nonZerosEachCol[c].push_back(arr[r][c]);
+                arr[r][c] = 0;
+            }
+        }
+    }
 
-	for (int c = 0; c < 10; c++) {
-		int rowIdx = rowMax - 1;
-		for (int i = 0; i < nonZerosEachCol[c].size(); i++) {
-			arr[rowIdx][c] = nonZerosEachCol[c][i];
-			rowIdx--;
-		}
-	}
+    for (int c = 0; c < 10; c++) {
+        int rowIdx = rowMax - 1;
+        for (int i = 0; i < nonZerosEachCol[c].size(); i++) {
+            arr[rowIdx][c] = nonZerosEachCol[c][i];
+            rowIdx--;
+        }
+    }
 
-	int de = 1;
+    int de = 1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie();
-	cout.tie();
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
 
-	cin >> rowMax >> K;
-	for (int r = 0; r < rowMax; r++) {
-		string s;
-		cin >> s;
-		for (int i = 0; i < 10; i++) {
-			arr[r][i] = (s[i] - '0');
-			visited[r][i] = 0;
-		}
-	}
+    cin >> rowMax >> K;
+    for (int r = 0; r < rowMax; r++) {
+        string s;
+        cin >> s;
+        for (int i = 0; i < 10; i++) {
+            arr[r][i] = (s[i] - '0');
+            visited[r][i] = 0;
+        }
+    }
 
-	// row는 밑에서 시작
+    // row는 밑에서 시작
+    int cntBomb = 1;
+    int flag = 1;
 
-	int cntBomb = 1;
-	int flag = 1;
-	while (flag) {
-		cntBomb = 1;
-		for (int row = rowMax - 1; row >= 0; row--) {
-			for (int col = 0; col < 10; col++) {
-				if (arr[row][col] == 0) continue;
-				{
-					// 들어와서 dfs 
-					checkNum = arr[row][col];
-					if (visited[row][col] != 0)
-						continue;
-					visited[row][col] = checkNum;
-					path.push_back({ row, col });
-					dfs({ row, col });
+    while (flag) {
+        cntBomb = 1;
+        initVisited();
+        for (int row = rowMax - 1; row >= 0; row--) {
+            for (int col = 0; col < 10; col++) {
+                if (arr[row][col] == 0)
+                    continue;
+                else{
+                    // 들어와서 dfs 
+                    checkNum = arr[row][col];
+                    if (visited[row][col] != 0)
+                        continue;
+                    visited[row][col] = checkNum;
+                    dfs({ row, col });
 
-					// 펑!
-					cout << '\n' << '\n';
-					path.pop_back();
-					if (path.size() >= K) {
-						for (int i = 0; i < path.size(); i++) {
-							Pos curr = path[i];
-							arr[curr.row][curr.col] = 0;
-						}
-						cntBomb++;
-					}
-					//path 초기화
-					while (!path.empty())
-						path.pop_back();
-					int forDebug = 1;
+                    // 펑!
+                    if (path.size() >= K) {
+                        for (int i = 0; i < path.size(); i++) {
+                            Pos curr = path[i];
+                            arr[curr.row][curr.col] = 0;
+                        }
+                        cntBomb++;
+                    }
+                    //path 초기화
+                    while (!path.empty())
+                        path.pop_back();
+                }
+            }
 
-					for (int r = 0; r < rowMax; r++) {
-						for (int c = 0; c < 10; c++) {
-							cout << arr[r][c];
-						}
-						cout << '\n';
-					}
+        }
+        doGravity();
+        if (cntBomb == 1)
+            break;
+    }
 
-				}
-			}
-			// 중력 + visited 초기화
-			initVisited();
-			if (cntBomb > 1)
-				doGravity();
-		}
-		if (cntBomb == 1)
-			break;
-	}
-
-	for (int r = 0; r < rowMax; r++) {
-		for (int c = 0; c < 10; c++)
-			cout << arr[r][c];
-		cout << '\n';
-	}
-	return 0;
+    for (int r = 0; r < rowMax; r++) {
+        for (int c = 0; c < 10; c++)
+            cout << arr[r][c];
+        cout << '\n';
+    }
+    return 0;
 }
