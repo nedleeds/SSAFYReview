b084e4bbaf3bf7efe084e845a3620a3fb6b7e86b
nedleeds <lllee.dh@gmail.com>
2022-09-21 17:58:01 +0900

    Fix : Linux week1 day4

---
+++ linux/test.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string>
+int main(){	
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%d, %d");
+
+	return 0;
+}
---
+++ linux/week1/0921/06_SymbolicLink/cat.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	system("nyancat");
+
+	return 0;
+}
