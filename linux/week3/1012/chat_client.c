bf8f91fba38c3016f4c5e0f657f7b585d2867791
dhl <lllee.dh@gmail.com>
2022-10-12 14:12:23 +0900

    Fix : Embedded week 1012 (point -> chat_server,client)

--- linux/week3/1012/chat_client.c
+++ linux/week3/1012/chat_client.c
@@ -12,33 +12,39 @@
 #define NAME_SIZE 20
 #define MSG_SIZE 100
 
-// AWS IP
+// SERVER IP & PORT
 const char *IP = "127.0.0.1";
 const char *PORT = "12345";
 
 pthread_t send_tid;
 pthread_t receive_tid;
-int exitFlag;
-int sock;
+int exitFlag; // thread 간 종료 상태 체크용
+int sock;  	  // socket
 
 // 채팅창에 보여질 이름의 형태
-char name[NAME_SIZE] = "[DEFAULT]";
-char msg[MSG_SIZE];
+// 개발자가 코드를 열었을 때 확인하기 위함
+char name[NAME_SIZE] = "[DEFAULT]"; // max: 20
+char msg[MSG_SIZE]; 				// max:100
 
 void *sendMsg()
 {
+	// data parsing: 미리 사이즈 할당
+	// -> NAME과 MSF 크기 모두 고려 (효율적)
 	char buf[NAME_SIZE + MSG_SIZE];
 
 	// exitFlag 는, 다른 한 쪽의 쓰레드를 종료하는 용도
 	while (!exitFlag)
 	{
+		// MSG 들어오면 초기화 진행
 		memset(buf, 0, NAME_SIZE + MSG_SIZE);
-		// 채팅 프로그램이므로, 띄어쓰기는 반드시 들어가야
+		// 띄어쓰기는 반드시 들어가야 -> fgets 사용
 		// 그래서 fgets 사용하며, 엔터까지 입력됨을 유의하라
 		fgets(msg, MSG_SIZE, stdin);
 		if (!strcmp(msg, "exit\n"))
 		{
+			// thread 종료 깃발 올림
 			exitFlag = 1;
+			// 서버에 메세지를 적음(보냄) 
 			write(sock, msg, strlen(msg));
 			break;
 		}
@@ -47,6 +53,7 @@ void *sendMsg()
 		// 실제로 서버에 보낼 메세지 형태
 		// buf = "[ssafy] 안녕 얘들아"
 		sprintf(buf, "%s %s", name, msg);
+		// 서버에 데이터 적음(보냄)
 		write(sock, buf, strlen(buf));
 	}
 }
@@ -57,11 +64,13 @@ void *receiveMsg()
 	while (!exitFlag)
 	{
 		memset(buf, 0, MSG_SIZE);
+		// server 에서 메세지 읽음(대기)
 		int len = read(sock, buf, NAME_SIZE + MSG_SIZE - 1);
 		if (len == 0)
 		{
+			// server 에서 종료한 경우
 			printf("INFO :: Server Disconnected\n");
-			kill(0, SIGINT);
+			kill(0, SIGINT); // interrupt 핸들러 진행
 			exitFlag = 1;
 			break;
 		}
@@ -74,11 +83,15 @@ void interrupt(int arg)
 	printf("\nYou typped Ctrl + C\n");
 	printf("Bye\n");
 
+	// thread 강제 종료 - 둘 다
 	pthread_cancel(send_tid);
 	pthread_cancel(receive_tid);
+	// 종료 대기 + 메모리 할당 해제
 	pthread_join(send_tid, 0);
 	pthread_join(receive_tid, 0);
 
+	// 종료가 되고 나면
+	// 소켓을 깔끔하게 닫는다
 	close(sock);
 	exit(1);
 }
@@ -96,6 +109,7 @@ int main(int argc, char *argv[])
 	}
 
 	// sprintf 사용해서, [닉네임] 형태로 name 저장
+	// python 의 f"{argv[1]}" 와 동일
 	sprintf(name, "[%s]", argv[1]);
 
 	sock = socket(PF_INET, SOCK_STREAM, 0);
@@ -105,12 +119,15 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	// sockaddr 로 형변환
 	struct sockaddr_in addr = {0};
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr(IP);
+	// inet_addr : str -> int, int -> long int, 빅엔디안
 	addr.sin_port = htons(atoi(PORT));
 
 	// Connect
+	// sock(int): File descriptort
 	if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
 	{
 		printf("ERROR :: 2_Connect Error\n");
