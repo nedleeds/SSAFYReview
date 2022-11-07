a96701369e682e82777221364afe974cbc822145
will <lllee.dh@gmail.com>
2022-11-07 21:47:42 +0900

    Fix : Embedded week7 PyQT 1107 add-assignment

--- linux/week7/1107/10_qt_assignment.py
+++ linux/week7/1107/10_qt_assignment.py
@@ -1,67 +1,86 @@
 from PySide6.QtWidgets import *
+from PySide6.QtGui import *
 
 
-class QT_OBJ():
-    def __init__(self):
-        self.app = QApplication()
-        self.win = QMainWindow()
-        self.status_bar = self.win.status_bar()
-        self.menu = {}
+def add_person():
+    global people_list, name, win
+    status = win.statusBar()
+    person = name.text()
 
-    def set_widget(self, title, pos):
-        self.win.setWindowTitle(title)
-        self.win.setGeometry(pos[0], pos[1], pos[2], pos[3])
-
-    def set_menu_bar(self, mode="추가"):
-        menu = self.win.menuBar()
-        self.menu[mode] = menu.addMenu(mode)
-
-    def set_status_bar(self, status):
-        self.status_bar.showMessage(status)
-
-    def set_center_menu(self):
-        '''
-        전체 layout 
-        main : vertical
-        name_line = QlineEdit()
-        btn = {
-            'add': QPushButton("추가")
-            'del': QPushButton("제거")
-        }
-        '''
-        layout = {
-            'menu': QVBoxLayout(),
-            'button': QHBoxLayout()
-        }
-        # QForm 의 addRow 메서드: label + widget 동시에 추가
-        form = QFormLayout()
-        
-        # layout 설정
-        label_wid = QWidget()
-        self.win.setCentralWidget(label_wid)
-        label = QLabel("\t인맥을 관리합시다.", label_wid)
-        label.adjustSize()
-        form.addRow(label)
-        
-        # line 설정
-        name_line = QLineEdit()
-        form.addRow("name", name_line)
-
-        
+    if person in people_list:
+        status.showMessage(f"{person}님은 동명이인이십니다")
+    elif len(person) == 0:
+        status.showMessage(f"이름을 입력하세요")
+    else:
+        people_list.append(person)
+        status.showMessage(f"{person}님이 등록되셨습니다")
+    return
 
 
-def main():
-    qt = QT_OBJ()
-    
-    qt.set_widget(title="인맥 관리 프로그램", pos=(400, 150, 700, 450))
-    qt.set_menu_bar("추가")
-    qt.set_menu_bar("제거")
-    qt.set_status_bar("인맥을 관리합니다")
-    qt.set_center_menu()
+def del_person():
+    global people_list, name, win
+    status = win.statusBar()
+    person = name.text()
 
+    if person not in people_list and len(people_list):
+        status.showMessage(f"{person}님은 없습니다")
+    elif len(person) == 0:
+        status.showMessage(f"이름을 입력하세요")
+    elif len(people_list) == 0:
+        status.showMessage(f"당신은 현재 인맥이 없습니다만?")
+    else:
+        people_list.remove(person)
+        status.showMessage(f"{person}님이 인맥에서 제외되셨습니다")
+    return
 
-    qt.win.show()
-    qt.app.exec()
+# init
+people_list = []
+app = QApplication()
+win = QMainWindow()
+win.setGeometry(400, 380, 500, 400)
 
+# QMainWindow - menuBar
+menu = win.menuBar()
+menu_menu = menu.addMenu("메뉴")
+menu_exit = menu.addMenu("종료")
 
-main()
+# QMainWindow - menuBar - action
+do_add = QAction("추가", win)
+do_del = QAction("제거", win)
+menu_menu.addAction(do_add)
+menu_menu.addAction(do_del)
+do_add.triggered.connect(add_person)
+do_del.triggered.connect(del_person)
+
+# QMainWindow - CentralWidget
+frame = QWidget()
+win.setCentralWidget(frame)
+form = QFormLayout()
+
+# 1st element - label
+label = QLabel()
+label.setText("인맥을 관리합시다")
+form.addWidget(label)
+
+# 2nd element - label
+name = QLineEdit()
+form.addRow("name", name)
+
+# 3rd element - buttons("add/del")
+box = QHBoxLayout()
+btn_add = QPushButton("추가", frame)
+btn_del = QPushButton("제거", frame)
+btn_add.clicked.connect(add_person)
+btn_del.clicked.connect(del_person)
+box.addWidget(btn_add)
+box.addWidget(btn_del)
+form.addRow(box)
+
+frame.setLayout(form)
+
+# QMainWindow - statusBar
+bar = win.statusBar()
+bar.showMessage("인맥을 관리합니다")
+
+win.show()
+app.exec()
